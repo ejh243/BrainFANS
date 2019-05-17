e65a20ab0cffa68b941b1b2bced7b527a926d4a9
ejh243 <e.j.hannon@exeter.ac.uk>
2019-05-17 12:24:12 +0100

    convert to bam; run picard

--- ChipSeq/alignment.sh
+++ ChipSeq/alignment.sh
@@ -1,5 +1,7 @@
 ## Written by Eilis
 ## Takes trimmed reads and aligns to genome with bowtie2
+## converts to bam files
+## excludes duplicates 
 
 FQFILES=$(ls ${DATADIR}/*/11_trimmed/*.fq.gz)
 
@@ -18,6 +20,14 @@ do
   mkdir -p ${foldername}
   bowtie2 -x ${REFGENOME}/genome -U ${f} -S ${foldername}/${basename}.sam &> ${foldername}/${basename}.bowtie.log
  
+ ## convert to sam files
+ samtools view -bSo ${foldername}/${basename}.bam ${foldername}/${basename}.sam
+ rm ${foldername}/${basename}.sam
 done
+
+## remove duplicates
+java -jar ${PICARD}/picard.jar MarkDuplicates I=${foldername}/${basename}.bam O=${foldername}/${basename}_depDuplicated.bam M=${foldername}/${basename}_dupMetrics.txt REMOVE_DUPLICATES=TRUE
+
+
 
 
