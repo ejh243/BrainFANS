48c464e35da38c96b1279eca0ca03b366146fb5a
ejh243 <e.j.hannon@exeter.ac.uk>
2019-06-21 14:09:59 +0100

    tidy up

--- DNAm/calcQCMetrics.r
+++ DNAm/calcQCMetrics.r
@@ -12,14 +12,18 @@ if(file.exists(qcData)){
 	QCmetrics<-sampleSheet
 }
 
+## extract a few useful matrices
+rawbetas<-betas(gfile)[,]
+
 # calculate median M & U intensity
 if(!"M.median" %in% colnames(QCmetrics)){
 	
 	m_intensities<-methylated(gfile)
 	u_intensities<-unmethylated(gfile)
-	M.median<-apply.gdsn(m_intensities, 2, median)
-	U.median<-apply.gdsn(u_intensities, 2, median)
+	M.median<-unlist(apply.gdsn(m_intensities, 2, median))
+	U.median<-unlist(apply.gdsn(u_intensities, 2, median))
 	QCmetrics<-cbind(QCmetrics,M.median, U.median)
+	
 }
 
 # calculate bisulfite conversion statistic
@@ -40,31 +44,62 @@ if(!"PC1_cp" %in% colnames(QCmetrics)){
 	pca <- prcomp(na.omit(ctrl.all))
 	ctrlprobes.scores = pca$x
 	colnames(ctrlprobes.scores) = paste(colnames(ctrlprobes.scores), '_cp', sep='')
-	QCmetrics<-cbind(QCmetrics,ctrlprobes.scores)
+	## only store the first 20
+	QCmetrics<-cbind(QCmetrics,ctrlprobes.scores[,1:20])
 }
 
-# Identiify outlier samples
-rawbetas<-betas(gfile)
-outlyx<- outlyx(rawbetas, plot = FALSE)
-QCmetrics<-cbind(QCmetrics,outlyx)
+## perform PCA on beta values
+if(!"PC1_betas" %in% colnames(QCmetrics)){	
 
-# detection p value filtering
+	pca <- prcomp(na.omit(rawbetas))
+	betas.scores = pca$x
+	colnames(betas.scores) = paste(colnames(betas.scores), '_betas', sep='')
+	## only store the first 20
+	QCmetrics<-cbind(QCmetrics,betas.scores[,1:20])
+}
+
+## Identiify outlier samples
+if(!"iqr" %in% colnames(QCmetrics)){
+	outlierDetect<- outlyx(rawbetas, plot = FALSE)
+	QCmetrics<-cbind(QCmetrics,outlierDetect)
+}
+
+## detection p value filtering
 pfilter.gds(gfile)
 
 
-# calc Horvaths epigenetic age
-data(coef)
-DNAmAge<-agep(gfile, coef=coef)
-QCmetrics<-cbind(QCmetrics,DNAmAge)
 
-# check sex
-predictSex(gfile)
+## calc Horvaths epigenetic age
+if(!"DNAmAge" %in% colnames(QCmetrics)){	
+	data(coef)
+	DNAmAge<-agep(gfile, coef=coef)
+	QCmetrics<-cbind(QCmetrics,DNAmAge)
+}
+
+## check sex
+# idenitfy X chromosome probes
+if(!"predSex" %in% colnames(QCmetrics)){	
+	x.probes<-which(fData(gfile)$chr == "chrX")
+	predSex<-predictSex(gfile, x.probes)
+	QCmetrics<-cbind(QCmetrics,predSex)
+}
+
+## check duplicate samples using SNPs on array
+if(!exists("snpCor")){
+	rsbetas<-rawbetas[grep("rs", rownames(rawbetas)),]
+	snpCor<-cor(rsbetas, use = "pairwise.complete.obs")
+}
 
 # check effect of normalisation
-
+if(!"rmsd" %in% colnames(QCmetrics)){
+	dasen(gfile, node="normbeta")
+	normbetas<-index.gdsn(gfile, "normbeta")[,]
+	qualDat<-qual(rawbetas, normbetas)
+	QCmetrics<-cbind(QCmetrics,qualDat)
+}
 
 ## need to close gds file in order to open in another R session
 closefn.gds(gfile)
 
-
-save(QCmetrics, file = qcData)
+# save QC metrics and SNP correlations to generate QC report
+save(QCmetrics, snpCor, file = qcData)
