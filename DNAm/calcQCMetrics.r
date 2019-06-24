c89ee856ae792df472ecd787f4643a5f7fa29840
ejh243 <e.j.hannon@exeter.ac.uk>
2019-06-24 16:24:19 +0100

    add predict sex function

--- DNAm/calcQCMetrics.r
+++ DNAm/calcQCMetrics.r
@@ -12,6 +12,8 @@ if(file.exists(qcData)){
 	QCmetrics<-sampleSheet
 }
 
+QCmetrics$Age<-as.numeric(as.character(QCmetrics$Age))
+
 ## extract a few useful matrices
 rawbetas<-betas(gfile)[,]
 
@@ -44,25 +46,30 @@ if(!"PC1_cp" %in% colnames(QCmetrics)){
 	pca <- prcomp(na.omit(ctrl.all))
 	ctrlprobes.scores = pca$x
 	colnames(ctrlprobes.scores) = paste(colnames(ctrlprobes.scores), '_cp', sep='')
-	## only store the first 20
-	QCmetrics<-cbind(QCmetrics,ctrlprobes.scores[,1:20])
+	ctrl.pca<-pca$sdev^2/sum(pca$sdev^2)
+	## only save PCs which explain > 1% of the variance
+	QCmetrics<-cbind(QCmetrics,ctrlprobes.scores[,which(ctrl.pca > 0.01)])
 }
 
 ## perform PCA on beta values
-if(!"PC1_betas" %in% colnames(QCmetrics)){	
+if(!"PC1_betas" %in% colnames(QCmetrics)){
+	## filter to autosomal only
+	auto.probes<-which(fData(gfile)$chr != "chrX" & fData(gfile)$chr != "chrY")
 
-	pca <- prcomp(na.omit(rawbetas))
+	pca <- prcomp(t(na.omit(rawbetas[auto.probes,])))
 	betas.scores = pca$x
 	colnames(betas.scores) = paste(colnames(betas.scores), '_betas', sep='')
-	## only store the first 20
-	QCmetrics<-cbind(QCmetrics,betas.scores[,1:20])
+	betas.pca<-pca$sdev^2/sum(pca$sdev^2)
+	## only save PCs which explain > 1% of the variance
+	QCmetrics<-cbind(QCmetrics,betas.scores[,which(ctrl.pca > 0.01)])
 }
 
-## Identiify outlier samples
-if(!"iqr" %in% colnames(QCmetrics)){
-	outlierDetect<- outlyx(rawbetas, plot = FALSE)
-	QCmetrics<-cbind(QCmetrics,outlierDetect)
-}
+## Identify outlier samples 
+## excluded as comparable to PC filtering already included
+#if(!"iqr" %in% colnames(QCmetrics)){
+	#outlierDetect<- outlyx(rawbetas, plot = FALSE)
+	#QCmetrics<-cbind(QCmetrics,outlierDetect)
+#}
 
 ## detection p value filtering
 pfilter.gds(gfile)
@@ -80,8 +87,28 @@ if(!"DNAmAge" %in% colnames(QCmetrics)){
 # idenitfy X chromosome probes
 if(!"predSex" %in% colnames(QCmetrics)){	
 	x.probes<-which(fData(gfile)$chr == "chrX")
-	predSex<-predictSex(gfile, x.probes)
-	QCmetrics<-cbind(QCmetrics,predSex)
+	y.probes<-which(fData(gfile)$chr == "chrY")
+	ints.auto<-methylated(gfile)[c(x.probes, y.probes),]+unmethylated(gfile)[c(x.probes, y.probes),]
+	ints.X<-methylated(gfile)[x.probes,]+unmethylated(gfile)[x.probes,]
+	ints.Y<-methylated(gfile)[y.probes,]+unmethylated(gfile)[y.probes,]
+
+	x.cp<-colMeans(ints.X, na.rm = TRUE)/colMeans(ints.auto, na.rm = TRUE)
+	y.cp<-colMeans(ints.Y, na.rm = TRUE)/colMeans(ints.auto, na.rm = TRUE)
+	
+	## base prediction on y chromosome
+	predSex.y<-rep(NA, length(y.cp))
+	predSex.y[which(y.cp > 1)]<-"M"
+	predSex.y[which(y.cp < 1)]<-"F"
+	
+	## base prediction on x chromosome
+	predSex.x<-rep(NA, length(x.cp))
+	predSex.x[which(x.cp < 1)]<-"M"
+	predSex.x[which(x.cp > 1)]<-"F"
+	
+	## check for consistent prediction
+	predSex<-rep(NA, length(x.cp))
+	predSex[which(predSex.x == predSex.y)]<-predSex.x[which(predSex.x == predSex.y)]
+	QCmetrics<-cbind(QCmetrics,x.cp,y.cp,predSex.x, predSex.y, predSex)
 }
 
 ## check duplicate samples using SNPs on array
@@ -102,4 +129,4 @@ if(!"rmsd" %in% colnames(QCmetrics)){
 closefn.gds(gfile)
 
 # save QC metrics and SNP correlations to generate QC report
-save(QCmetrics, snpCor, file = qcData)
+save(QCmetrics, snpCor, betas.pca, ctrl.pca, file = qcData)
