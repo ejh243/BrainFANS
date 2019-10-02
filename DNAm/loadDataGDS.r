bfa5e46c432599b52508e8003ffb1b9f42bad45a
ejh243 <e.j.hannon@exeter.ac.uk>
2019-10-02 16:52:41 +0100

    fix option to update existing gfile

--- DNAm/loadDataGDS.r
+++ DNAm/loadDataGDS.r
@@ -2,18 +2,8 @@
 ## R script to load data from idats files into a gds file
 ## requires a sample sheet with Basename or Chip.ID and Chip.Location to identify which samples to load
 ## script to load DNAm data from idat files listed in a sample sheet into gds file using bigmelon
-## NB if gds file already exists it is deleted and recreated
+## NB if gds file already exists option to delete and recreate
 args<-commandArgs(trailingOnly = TRUE)
-
-iadd <- function (bar, gds, n=T, ...){
-    ifile <- basename(bar)
-    pieces <- strsplit(ifile, "[_.]")
-    slide <- sapply(pieces, '[', 1)
-    pos <- sapply(pieces, '[', 2)
-    bar <- paste(slide, pos, sep = "_")
-    mlu <- methylumIDATepic(barcodes = bar, n=n, ...)
-    app2gds(mlu, gds)
-}
 
 library(bigmelon)
 library(IlluminaHumanMethylationEPICanno.ilm10b2.hg19)
@@ -33,6 +23,10 @@ if(file.exists(gdsFile) & recreate == TRUE){
 		gfile<-openfn.gds(gdsFile, readonly = FALSE, allow.fork = TRUE)
 		sampToLoad<-sampleSheet$Basename[!sampleSheet$Basename %in% read.gdsn(index.gdsn(gfile, "pData/Basename"))]
 		closefn.gds(gfile)
+	}else {
+		if(file.exists(gdsFile)){
+		sampToLoad<-sampleSheet$Basename
+		}
 	}
 }
 
@@ -41,6 +35,8 @@ setwd("iDats")
 for(each in sampToLoad){
 	gfile <- iadd(bar = each, gds = paste("../", gdsFile, sep = "")) 
 }
+
+
 
 ## update feature data
 
@@ -58,10 +54,10 @@ add.gdsn(gfile, 'fData', val = data.frame(lapply(as.data.frame(newfData), as.cha
 
 ## update pData need to check all in the sample order, should be as loaded from sample sheet
 ## format sampleSheet to match order of gds file
-sampleOrder<-pData(gfile)$barcode
+sampleOrder<-names(gfile[1, node = 'betas', name = TRUE])
 sampleSheet<-sampleSheet[match(sampleOrder, sampleSheet$Basename),]
 
-sampleSheet<-cbind(pData(gfile)$barcode, sampleSheet)
+sampleSheet<-cbind(sampleSheet$Basename, sampleSheet)
 colnames(sampleSheet)[1]<-"barcode"
 add.gdsn(gfile, 'pData', val = data.frame(lapply(as.data.frame(sampleSheet), as.character), stringsAsFactors = FALSE), replace = TRUE)
 
