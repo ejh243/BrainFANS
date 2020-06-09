92aec06831e5e57efe3a81d7c6dd381530fe91b2
ejh243 <e.j.hannon@exeter.ac.uk>
2020-06-09 12:59:34 +0100

    update pipeline for SE

--- JobSubmissionScripts/runChipSeqAlignmentSE.sh
+++ JobSubmissionScripts/runChipSeqAlignmentSE.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #PBS -V # export all environment variables to the batch job.
 #PBS -q sq # submit to the serial queue
-#PBS -l walltime=60:00:00 # Maximum wall time for the job.
+#PBS -l walltime=150:00:00 # Maximum wall time for the job.
 #PBS -A Research_Project-MRC190311 # research project to submit under. 
 #PBS -l procs=1 # specify number of processors.
 #PBS -m e -M e.j.hannon@exeter.ac.uk # email me at job completion
-#PBS -e LogFiles/ChipAlignmentADRun2.err # error file
-#PBS -o LogFiles/ChipAlignmentADRun2.log # output file
+#PBS -e LogFiles/ChipAlignmentAD.err # error file
+#PBS -o LogFiles/ChipAlignmentAD.log # output file
 
 ## Output some useful job information
 
