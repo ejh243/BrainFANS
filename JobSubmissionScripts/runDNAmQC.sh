bff4da29d3300901912073b1621ff1baf8d070ed
ejh243 <e.j.hannon@exeter.ac.uk>
2021-07-15 11:54:10 +0100

    update to SLURM

--- JobSubmissionScripts/runDNAmQC.sh
+++ JobSubmissionScripts/runDNAmQC.sh
@@ -1,32 +1,31 @@
 #!/bin/sh
-#PBS -V # export all environment variables to the batch job.
-#PBS -q sq # submit to the serial queue
-#PBS -l walltime=10:00:00 # Maximum wall time for the job.
-#PBS -A Research_Project-MRC190311 # research project to submit under. 
-#PBS -l procs=1 # specify number of processors.
-#PBS -m e -M e.j.hannon@exeter.ac.uk # email me at job completion
-#PBS -e ../LogFiles/QCDNAdata.err # error file
-#PBS -o ../LogFiles/QCDNAdata.log # output file
-
+#SBATCH --export=ALL #export all enviroment variables to the batch job
+#SBATCH -p mrcq #submit to the serial queue
+#SBATCH --time=24:00:00 ##maximum wall time for the job
+#SBATCH -A Research_Project-MRC190311 #research project to submit under
+#SBATCH --nodes=8 #specify number of nodes
+#SBATCH --ntasks-per-node=16 # specify number of processors per node
+#SBATCH --mail-type=END # send email at job completion
+#SBATCH --mail-user=e.j.hannon@exeter.ac.uk # email me at job completion
+#SBATCH --error=../LogFiles/QCDNAdata.err # error file
+#SBATCH --output=../LogFiles/QCDNAdata.log # output file
+#SBATCH --job-name=QCDNAdata
 
 ## Output some useful job information
-
-echo PBS: working directory is $PBS_O_WORKDIR
-echo PBS: job identifier is $PBS_JOBID
-echo PBS: job name is $PBS_JOBNAME
-echo PBS: current home directory is $PBS_O_HOME
 
 ## print start date and time
 echo Job started on:
 date -u
 
+echo "Changing Folder to: "
+echo $SLURM_SUBMIT_DIR
 
-cd $PBS_O_WORKDIR
+cd $SLURM_SUBMIT_DIR
 
 module load Pandoc
 module load R/3.6.0-foss-2019a
 
-Rscript performQC.r rmdConfig.sfari 
+Rscript performQC.r rmdConfig.mrc 
 
 Rscript -e "rmarkdown::render('QC.rmd', params = list(config='rmdConfig.mrc'), output_file='../../DNAm/QCmetrics/QC.html')"
 
