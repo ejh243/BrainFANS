02d0c3c6bde05a4d5813a96fa47f0feb8bfb538b
ejh243 <e.j.hannon@exeter.ac.uk>
2021-08-02 09:43:06 +0100

    reformat to submit as batch array job

--- ATACSeq/alignmentPE.sh
+++ ATACSeq/alignmentPE.sh
@@ -1,64 +1,80 @@
-## Written by Eilis	
+## script to process a single sample
+## Requires a raw (r1) fastq file provided on the command line 
 ## Takes trimmed reads and aligns to genome with bowtie2	
 ## converts to bam files	
 ## excludes duplicates, mt reads, only keeps properly paired reads
 ## shifts read prior to peak calling
 
+
 CHR=(chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX)
+
+f=$1 
 cd ${DATADIRPE}
 
-RAWDATADIR=($(find . -name '11_trimmed'))
+## extract sample names
+FOLDER=$(dirname ${f})
+f1=$(basename $f)
+sampleName=${f1%_[rR]*}
+## later samples have an additional _S[num] in the file name need to remove
+sampleName=${sampleName%_S[0-9]*}
+echo "Processing" ${sampleName}
 
-for FOLDER in ${RAWDATADIR[@]}
-do
-	echo "Processing:"
-	echo ${FOLDER}
-	FQFILES=($(ls ${FOLDER}/*[rR]1*q.gz))	
-	echo "Number of R1 .fq.gz files found for alignment:"" ""${#FQFILES[@]}"""	
+## need trimmed files for alignment
+FOLDERTRIM=${FOLDER/01_raw_reads/11_trimmed}
 
-	mkdir -p ${ALIGNEDDIR}	
-		
-	for f in ${FQFILES[@]};	
-	do	
-	  echo "Aligning"" ${f}"	
-	  basename=$(basename $f)
-	  sampleName=${basename/_trimmed_r1.fq.gz}
-	  pairedFiles=($(ls ${FOLDER}/${sampleName}*.gz))
-	  f1=${pairedFiles[0]}
-	  f2=${pairedFiles[1]}
+## filename format changed sometime r1_trimmed sometimes trimmed_r1
+if ls ${FOLDERTRIM}/${sampleName}*[rR]1*_trimmed.f* 1> /dev/null 2>&1;
+then 
+  f1=$(basename $(ls ${FOLDERTRIM}/${sampleName}*[rR]1*_trimmed.f*))
+  f2=$(basename $(ls ${FOLDERTRIM}/${sampleName}*[rR]2*_trimmed.f*))
+else
+  f1=$(basename $(ls ${FOLDERTRIM}/${sampleName}*_trimmed_[rR]1*.f*))
+  f2=$(basename $(ls ${FOLDERTRIM}/${sampleName}*_trimmed_[rR]2*.f*))
+fi
 
-	  if [ ! -f ${sampleName}_hist.txt ]	
-	  then
-		## count uniqueness
-		${BBMAP}/bbcountunique.sh in=${f1} in2=${f2} out=${sampleName}_hist.txt interval=5000 overwrite=true cumulative=true count=t
-	  fi
+echo "Found trimmed files:"
+echo ${f1}
+echo ${f2}
+
+## checks if last file exists and is not empty
+if [ ! -s ${ALIGNEDDIR}/${sampleName}_postFilter_statsperchr.txt ]	
+	then
+	echo "Running alignment for"" ${sampleName}"
+	date -u	
+	## alignment
+	bowtie2  -p 10 -X 1000 -k ${multimap} -x ${REFGENOME}/genome -1 ${FOLDERTRIM}/${f1} -2 ${FOLDERTRIM}/${f2} -S ${ALIGNEDDIR}/${sampleName}.sam &> ${ALIGNEDDIR}/${sampleName}.bowtie.log
+
+	## convert to sam files, sort and index
+	echo "converting to sam file, sorting and indexing"
+	samtools view -bSo ${ALIGNEDDIR}/${sampleName}.bam ${ALIGNEDDIR}/${sampleName}.sam
+	samtools sort ${ALIGNEDDIR}/${sampleName}.bam > ${ALIGNEDDIR}/${sampleName}_sorted.bam
+	samtools index ${ALIGNEDDIR}/${sampleName}_sorted.bam
+	samtools idxstats ${ALIGNEDDIR}/${sampleName}_sorted.bam > ${ALIGNEDDIR}/${sampleName}_statsperchr.txt
+	rm ${ALIGNEDDIR}/${sampleName}.bam
+	rm ${ALIGNEDDIR}/${sampleName}.sam
+
+	## extract chr1 for calculation of ENCODE metrics
+	echo "extracting chr 1 for ENCODE metrics calculations"
+	samtools view -b ${ALIGNEDDIR}/${sampleName}_sorted.bam chr1 > ${ALIGNEDDIR}/${sampleName}_sorted_chr1.bam
+	
+	## remove reads from MT and other random chrs
+	echo "removing MT chr"
+	samtools view -b ${ALIGNEDDIR}/${sampleName}_sorted.bam ${CHR[@]} > ${ALIGNEDDIR}/${sampleName}_noMT.bam	
+
+	## remove duplicates
+	echo "removing duplicates"
+	java -jar $EBROOTPICARD/picard.jar MarkDuplicates I=${ALIGNEDDIR}/${sampleName}_noMT.bam O=${ALIGNEDDIR}/${sampleName}_depDuplicated.bam M=${ALIGNEDDIR}/${sampleName}_dupMetrics.txt REMOVE_DUPLICATES=TRUE
 	  
-	  if [ ! -f ${ALIGNEDDIR}/${sampleName}_postFilter_statsperchr.txt ]	
-	  then
-		## alignment
-		bowtie2  -p 10 -k ${multimap} -x ${REFGENOME}/genome -1 ${f1} -2 ${f2} -S ${ALIGNEDDIR}/${sampleName}.sam &> ${ALIGNEDDIR}/${sampleName}.bowtie.log
-		
-		## convert to sam files, sort and index
-		samtools view -bSo ${ALIGNEDDIR}/${sampleName}.bam ${ALIGNEDDIR}/${sampleName}.sam
-		samtools sort ${ALIGNEDDIR}/${sampleName}.bam > ${ALIGNEDDIR}/${sampleName}_sorted.bam
-		samtools index ${ALIGNEDDIR}/${sampleName}_sorted.bam
-		samtools idxstats ${ALIGNEDDIR}/${sampleName}_sorted.bam > ${ALIGNEDDIR}/${sampleName}_statsperchr.txt
-		rm ${ALIGNEDDIR}/${sampleName}.bam
-		rm ${ALIGNEDDIR}/${sampleName}.sam
+	## remove reads with q < 30;only keep properly paired reads; exclude reads marked as pcr optical duplicate, or secondary alignment
+	echo "filtering aligned reads"
+	samtools view -f 0x2 -b -F 0x400 -F 0x100 -q 30 -h ${ALIGNEDDIR}/${sampleName}_depDuplicated.bam > ${ALIGNEDDIR}/${sampleName}_depDup_q30.bam
+	rm ${ALIGNEDDIR}/${sampleName}_depDuplicated.bam
+	rm ${ALIGNEDDIR}/${sampleName}_noMT.bam
 
-		## remove reads from MT and other random chrs
-		samtools view -b ${ALIGNEDDIR}/${sampleName}_sorted.bam ${CHR[@]} > ${ALIGNEDDIR}/${sampleName}_noMT.bam	
-		
-		## remove duplicates
-		java -jar $EBROOTPICARD/picard.jar MarkDuplicates I=${ALIGNEDDIR}/${sampleName}_noMT.bam O=${ALIGNEDDIR}/${sampleName}_depDuplicated.bam M=${ALIGNEDDIR}/${sampleName}_dupMetrics.txt REMOVE_DUPLICATES=TRUE
-		  
-		## remove reads with q < 30;only keep properly paired reads; exclude reads marked as pcr optical duplicate, or secondary alignment
-		samtools view -f 0x2 -b -F 0x400 -F 0x100 -q 30 -h ${ALIGNEDDIR}/${sampleName}_depDuplicated.bam > ${ALIGNEDDIR}/${sampleName}_depDup_q30.bam
-		rm ${ALIGNEDDIR}/${sampleName}_depDuplicated.bam
-		rm ${ALIGNEDDIR}/${sampleName}_noMT.bam
-		
-		samtools index ${ALIGNEDDIR}/${sampleName}_depDup_q30.bam
-	  samtools idxstats ${ALIGNEDDIR}/${sampleName}_depDup_q30.bam > ${ALIGNEDDIR}/${sampleName}_postFilter_statsperchr.txt
-	  fi	
-	done
-done
+	samtools index ${ALIGNEDDIR}/${sampleName}_depDup_q30.bam
+	samtools idxstats ${ALIGNEDDIR}/${sampleName}_depDup_q30.bam > ${ALIGNEDDIR}/${sampleName}_postFilter_statsperchr.txt
+fi
+
+echo "Alignment and post filtering complete"
+date -u
+
---
+++ JobSubmissionScripts/batchRunATACAlignment.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+#SBATCH --export=ALL # export all environment variables to the batch job.
+#SBATCH -p sq # submit to the serial queue
+#SBATCH --time=24:00:00 # Maximum wall time for the job.
+#SBATCH -A Research_Project-193495 # research project to submit under. 
+#SBATCH --nodes=1 # specify number of nodes.
+#SBATCH --ntasks-per-node=16 # specify number of processors per node
+#SBATCH --mail-type=END # send email at job completion 
+#SBATCH --mail-user=e.j.hannon@exeter.ac.uk # email me at job completion
+#SBATCH --output=LogFiles/ATAQAlignment-%A_%a.o
+#SBATCH --error=LogFiles/ATAQAlignment-%A_%a.e
+#SBATCH --job-name=ATAQAlignment-%A_%a.e
+#SBATCH --array=0-40%40 ## runs 19 jobs with 10 at any one time
+
+## print start date and time
+echo Job started on:
+date -u
+
+## needs to be executed from the scripts folder
+echo "Changing Folder to: "
+echo $SLURM_SUBMIT_DIR
+
+cd $SLURM_SUBMIT_DIR
+
+## load config file provided on command line when submitting job
+echo "Loading config file: "
+echo $1
+source ./$1 
+
+echo "Changing Folder to Data directory "
+echo ${DATADIRPE}
+
+cd ${DATADIRPE}
+## find all folders with fastq files in
+RAWDATADIR=($(find . -name '01_raw_reads'))
+
+## create array of all fastq files
+FQFILES=()
+for FOLDER in ${RAWDATADIR[@]}
+do 	
+    echo "Processing folder: "
+	echo ${FOLDER}
+	FQFILES+=($(find ${FOLDER} -name '*[rR]1*q.gz')) ## this command searches for all fq files within
+done
+
+echo "Number of R1 .fq.gz files found for alignment:"" ""${#FQFILES[@]}"""	
+
+sample=${FQFILES[${SLURM_ARRAY_TASK_ID}]}
+sampleName=$(basename ${sample%_[rR]*})
+
+## run sequencing QC and trimming on fastq files		
+module load FastQC 
+module load fastp
+
+cd ${SCRIPTDIR}
+sh ./ATACSeq/qcRawData.sh ${sample}  
+
+## run alignment and post processing on sample
+module purge ## had conflict issues if this wasn't run first
+module load Bowtie2/2.3.4.2-foss-2018b
+module load SAMtools
+module load picard/2.6.0-Java-1.8.0_131
+
+cd ${SCRIPTDIR}
+sh ./ATACSeq/alignmentPE.sh ${sample}
+
+module purge
+module load SAMtools
+module load picard/2.6.0-Java-1.8.0_131
+module load BEDTools
+export PATH="$PATH:/gpfs/mrc0/projects/Research_Project-MRC190311/software/atac_dnase_pipelines/utils/"
+
+
+mkdir -p ENCODEMetrics
+
+cd ${SCRIPTDIR}
+./ATACSeq/calcENCODEQCMetricsPE.sh ${sampleName}_sorted_chr1.bam
