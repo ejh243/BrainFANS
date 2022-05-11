86ca020f4ee1eb29259ad9e584e65f288633ead0
ejh243 <e.j.hannon@exeter.ac.uk>
2022-05-11 13:21:57 +0100

    add in liftover step

--- array/SNPArray/preprocessing/7_combineImputationOutput.sh
+++ array/SNPArray/preprocessing/7_combineImputationOutput.sh
@@ -24,40 +24,36 @@
 cd $1
 for i in {1..22}
 do
-   ## identify variants with rsq > 0.3 to keep
-   gzip -cd  chr$i.info.gz | awk '{if($7 > 0.3) print $1,$7}' > chr$i.keep
-   vcftools --gzvcf chr$i.dose.vcf.gz --plink --chr $i --out chr$i
+
+   # filter on rsq > 0.3 and MAF > 0.01(https://www.biostars.org/p/205856/)
+	bcftools view -i 'R2>.3 & MAF>0.01' -Ov chr${i}.dose.hg38.vcf.gz > chr${i}_filt.vcf
+	vcftools --vcf chr${i}_filt.vcf --plink --chr chr$i --out chr$i 
+	rm chr${i}_filt.vcf
    
-   ## convert to bim,bed,fam filter variants to MAF > 0.01
-   ${PLINK}/plink --ped chr$i.ped --map chr$i.map --extract chr$i.keep --maf 0.01 --make-bed --out chr${i}_rsq0.3 
+   ## convert to bim,bed,fam 
+   ${PLINK}/plink --ped chr$i.ped --map chr$i.map --make-bed --out chr${i}_rsq0.3 
    rm chr$i.ped
    rm chr$i.map
 
 done
 
-# Merge them into one dataset
+# Merge vcf files into one
+
+vcf-concat chr*_filt.vcf | gzip -c > out.vcf.gz
+
+# Merge them into one plink dataset
 
 for i in {2..22}
 do 
     echo "chr${i}_rsq0.3"
 done > mergefile.txt
 
+
 ${PLINK}/plink --bfile chr1_rsq0.3 --merge-list mergefile.txt --make-bed --out ${FILEPREFIX}_rsq0.3
 
 rm chr*_rsq0.3.bed
 rm chr*_rsq0.3.bim
 rm chr*_rsq0.3.fam
-
-# Combine info files into a single file
-
-head -n1 chr1.keep > AllVariantsrsq0.3.info
-
-for i in {1..22}
-do
-    awk ' NR>1 {print $0}' < chr$i.keep |cat >> AllVariantsrsq0.3.info
-done
-
-rm chr*.keep
 
 
 ## update sex in fam file run hwe filter
