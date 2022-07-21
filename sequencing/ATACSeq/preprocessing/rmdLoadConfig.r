920b4c7eb514ad2630bf93c2e3d8d46dceff5ea8
ejh243 <e.j.hannon@exeter.ac.uk>
2022-07-21 15:15:43 +0100

    tidy up mrkdown

--- sequencing/ATACSeq/preprocessing/rmdLoadConfig.r
+++ sequencing/ATACSeq/preprocessing/rmdLoadConfig.r
@@ -8,3 +8,9 @@ if (is.na(params$project)){
     project<-params$project
   source(params$configFile)
 }
+
+
+## create colourblind friendly palette
+colorBlindGrey8   <- c("#009E73", "#CC79A7", "#D55E00", "#999999", 
+                       "#F0E442", "#0072B2",  "#E69F00", "#56B4E9")
+
