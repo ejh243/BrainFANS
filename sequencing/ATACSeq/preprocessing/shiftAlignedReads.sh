49d4c6fdb18a738537d0023bfcbdd73867374244
Jessica Shields <jess.m.shields@live.co.uk>
2023-03-29 15:22:34 +0100

    retain readnames

--- sequencing/ATACSeq/preprocessing/shiftAlignedReads.sh
+++ sequencing/ATACSeq/preprocessing/shiftAlignedReads.sh
@@ -32,34 +32,36 @@ cd ${ALIGNEDDIR}
 # ===================
 #
 # Create virtual SE file containing both read pairs
+#bedtools bamtobed -i ${sampleName}.filt.nodup.bam | awk 'BEGIN{OFS="\t"}{$4="N";$5="1000";print $0}' | gzip -c > ${sampleName}.PE2SE.tn5.tagAlign.gz
+
 bedtools bamtobed -i ${sampleName}.filt.nodup.bam | awk 'BEGIN{OFS="\t"}{$4="N";$5="1000";print $0}' | gzip -c > ${sampleName}.PE2SE.tn5.tagAlign.gz
 
 # ================ 
 # Create BEDPE file 
 # ================ 
 ## requires name sorted bam file
-samtools sort -n ${sampleName}.filt.nodup.bam > ${sampleName}.filt.nodup.nmsrt.bam
-bedtools bamtobed -bedpe -mate1 -i ${sampleName}.filt.nodup.nmsrt.bam | gzip -c > ${sampleName}.filt.nodup.nmsrt.bedpe.gz
-rm ${sampleName}.filt.nodup.nmsrt.bam
+#samtools sort -n ${sampleName}.filt.nodup.bam > ${sampleName}.filt.nodup.nmsrt.bam
+#bedtools bamtobed -bedpe -mate1 -i ${sampleName}.filt.nodup.nmsrt.bam | gzip -c > ${sampleName}.filt.nodup.nmsrt.bedpe.gz
+#rm ${sampleName}.filt.nodup.nmsrt.bam
 
 # =================================
 # Subsample tagAlign file
 # Restrict to one read end per pair for CC analysis
 # ================================
-zcat ${sampleName}.filt.nodup.nmsrt.bedpe.gz | grep -v “chrM” | shuf -n ${NREADS} | awk 'BEGIN{OFS="\t"}{print $1,$2,$3,"N","1000",$9}' | \
-	gzip -c > "${sampleName}.filt.nodup.sample.$((NREADS /1000000)).MATE1.tagAlign.gz"
+#zcat ${sampleName}.filt.nodup.nmsrt.bedpe.gz | grep -v “chrM” | shuf -n ${NREADS} | awk 'BEGIN{OFS="\t"}{print $1,$2,$3,"N","1000",$9}' | \
+#	gzip -c > "${sampleName}.filt.nodup.sample.$((NREADS /1000000)).MATE1.tagAlign.gz"
 
 # =================================
 # Calculate cross correlation scores
 # ================================
 # CC_SCORE FILE format
 # Filename <tab> numReads <tab> estFragLen <tab> corr_estFragLen <tab> PhantomPeak <tab> corr_phantomPeak <tab> argmin_corr <tab> min_corr <tab> phantomPeakCoef <tab> relPhantomPeakCoef <tab> QualityTag
-Rscript ${PHANTOMPEAK}/run_spp.R -c="${sampleName}.filt.nodup.sample.$((NREADS /1000000)).MATE1.tagAlign.gz" -p=${NTHREADS} \
-	-filtchr=chrM -savp=${sampleName}.subsample.cc.plot.pdf -out=${sampleName}.subsample.cc.qc -rf
-sed -r 's/,[^\t]+//g' ${sampleName}.subsample.cc.qc > temp
-mv temp ${sampleName}.subsample.cc.qc
+#Rscript ${PHANTOMPEAK}/run_spp.R -c="${sampleName}.filt.nodup.sample.$((NREADS /1000000)).MATE1.tagAlign.gz" -p=${NTHREADS} \
+#	-filtchr=chrM -savp=${sampleName}.subsample.cc.plot.pdf -out=${sampleName}.subsample.cc.qc -rf
+#sed -r 's/,[^\t]+//g' ${sampleName}.subsample.cc.qc > temp
+#mv temp ${sampleName}.subsample.cc.qc
 
-echo 'Calculated cross correlation'
+#echo 'Calculated cross correlation'
 
 #rm "${sampleName}.filt.nodup.sample.$((NREADS /1000000)).MATE1.tagAlign.gz"
 #rm ${sampleName}.filt.nodup.nmsrt.bedpe.gz
