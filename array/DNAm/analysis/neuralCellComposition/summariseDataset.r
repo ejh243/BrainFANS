1be5cc29793d9451a9181a1c4163e0a56d0277ec
ejh243 <e.j.hannon@exeter.ac.uk>
2023-06-01 13:09:13 +0100

    tidy up figures

--- array/DNAm/analysis/neuralCellComposition/summariseDataset.r
+++ array/DNAm/analysis/neuralCellComposition/summariseDataset.r
@@ -98,7 +98,7 @@ colnames(pcDatAll)[11]<-"Cell.type"
 
 
 fig2<-list()
-fig2[[1]]<-ggplot(pcDatAll, aes(x=PC1, y = PC2, color = Cell.type)) + geom_point(size = 4) + scale_color_manual(values = ctCols[sort(subCells)],
+fig2[[1]]<-ggplot(pcDatAll, aes(x=PC1, y = PC2, color = Cell.type)) + geom_point(size = 4) + scale_color_manual(values = ctCols,
                            guide = guide_legend(override.aes = list(shape = 15, size = 10) )) + theme(text=element_text(size=21), legend.direction="horizontal") + labs(tag = letters[1]) + labs(color=NULL)
 
 
@@ -108,9 +108,9 @@ fig2[[i+1]]<-ggplot(pcDatAll, aes_string(x="Cell.type", y=paste0("PC",i), fill =
 		  stat_summary(fun = "mean", 
 					   geom = "point", 
 					   position = pos, col = "white")  + theme(legend.position = "none", text=element_text(size=21), axis.text.x = element_blank()) +
-					   scale_fill_manual(values = ctCols[sort(subCells)]) + labs(tag = letters[i+1]) + xlab("")
+					   scale_fill_manual(values = ctCols) + labs(tag = letters[i+1]) + xlab("")
 }
 
 
-ggarrange(plotlist=fig2, nrow = 2, ncol = 3, common.legend = TRUE, legend.grob = get_legend(fig1[[1]], position = NULL))
+ggarrange(plotlist=fig2, nrow = 2, ncol = 3, common.legend = TRUE, legend.grob = get_legend(fig2[[1]], position = NULL))
 ggsave(filename = file.path(plotPath, paste0("PCsByCellFractionCombined.pdf")),  units = "in", width = 18, height = 10)
