8f8ff2dd4c7aaeea1b00fa7d33c16509654fedeb
Haigh <rh829@login01.cluster.local>
2023-08-18 14:48:02 +0100

    Formatting edits

--- array/DNAm/preprocessing/installLibraries.r
+++ array/DNAm/preprocessing/installLibraries.r
@@ -1,16 +1,34 @@
-##INSTALL LIBRARIES SCRIPT
-##Installs all R packages required DNAm QC pipeline
-#Add in additional lines for Alice's errors... Git and config sourcing?
+##---------------------------------------------------------------------#
+##
+## Title: Install required packages
+##
+## Purpose of script: installs all packages required for QC pipeline steps.
+##
+## Author: Rhiannon Haigh
+##
+## Date Created: 08/2023
+##
+##---------------------------------------------------------------------#
 
-#Bioconductor is required for most installations
+
+#---------------------------------------------------------------------#
+# INSTALL BIOCONDUCTOR
+#---------------------------------------------------------------------#
+
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
-	
+
+#---------------------------------------------------------------------#
+# INSTALL BIGMELON
+#---------------------------------------------------------------------#	
 	
 #Bigmelon required for all scripts
 BiocManager::install("bigmelon")
 
-#GDS script
+#---------------------------------------------------------------------#
+# INSTALL PACKAGES FOR LOADING GDS FILES
+#---------------------------------------------------------------------#
+
 BiocManager::install("IlluminaHumanMethylationEPICanno.ilm10b4.hg19") #This one is commented out in original script so unsure if necessary
 BiocManager::install("IlluminaHumanMethylationEPICanno.ilm10b2.hg19")
 BiocManager::install("IlluminaHumanMethylationEPICmanifest")
@@ -18,11 +36,16 @@ BiocManager::install("IlluminaHumanMethylation450kanno.ilmn12.hg19")
 BiocManager::install("IlluminaHumanMethylation450kmanifest")
 install.packages("devtools")
 
-#Calculating QC metrics
+#---------------------------------------------------------------------#
+# INSTALL PACKAGES FOR QC METRICS
+#---------------------------------------------------------------------#
+
 install.packages("e1071")
 
+#---------------------------------------------------------------------#
+# INSTALL PACKAGES FOR BRAIN CELL PROPORTION PREDICTION
+#---------------------------------------------------------------------#
 
-#Additional packages for Brain Cell Proportion Prediction
 BiocManager::install("genefilter")
 install.packages("quadprog")
 
