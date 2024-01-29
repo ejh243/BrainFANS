eff3fd73cbacb5670c9167e7fba73d52e38d7fd2
sof202 <s.o.fletcher@exeter.ac.uk>
2024-01-29 16:25:56 +0000

    initial website configuration

---
+++ documentation/docs/home/intro.md
@@ -0,0 +1,8 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# Welcome to the BrainFANS wiki!
+
+This is the home page, use the navigation bar at the top of the page.
---
+++ documentation/docusaurus.config.js
@@ -0,0 +1,123 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'BrainFANS',
+  tagline: 'Documentation for BrainFANS repository',
+  favicon: 'img/dna.ico',
+
+  // Set the production url of your site here
+  url: 'https://ejh243.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/BrainFANS/',
+  trailingSlash: false,
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'ejh243', // Usually your GitHub org/user name.
+  projectName: 'BrainFANS', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.js',
+          path: 'docs',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/dna.png',
+      navbar: {
+        title: 'BrainFANS',
+        logo: {
+          alt: 'Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'homeSidebar',
+            position: 'left',
+            label: 'Home',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'documentationSidebar',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'contributingSidebar',
+            position: 'left',
+            label: 'How to contribute',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'analysesSidebar',
+            position: 'left',
+            label: 'Analyses',
+          },
+          {
+            href: 'https://github.com/ejh243/BrainFANS',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()}, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
