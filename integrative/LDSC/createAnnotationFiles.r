7465d5c9117672c7f84fb6cd179940485d76b5d3
sof202 <s.o.fletcher@exeter.ac.uk>
2024-02-29 12:16:57 +0000

    refactor with updated comments and removed repetition of lines

--- integrative/LDSC/createAnnotationFiles.r
+++ integrative/LDSC/createAnnotationFiles.r
@@ -1,98 +1,85 @@
-## need BEDTools and BEDOPs available on the command line
+## You need BEDTools and BEDOPs available on the command line for this script
+## to work properly.
 
-setwd("/gpfs/mrc0/projects/Research_Project-MRC190311/")
+configuration_file_path <- commandArgs(trailingOnly = TRUE)
+
+source(configuration_file_path)
 
 library(data.table)
 library(bedr)
 library(dplyr)
 
 
-## load ATAC peakSets
-allpeaks<-read.table("ATACSeq/CalledPeaks/AllData/OCT2020/AllFractions_peaks.broadPeak", stringsAsFactors = FALSE)
-allpeaks<-allpeaks[order(allpeaks[,1], allpeaks[,2]),]
-allRegions.atac<-bed2index(allpeaks[,1:3])
+read_peaks <- function(file_paths) {
+	sorted_peaks <- list()
 
-totalpeaks<-read.table("ATACSeq/CalledPeaks/AllData/OCT2020/T_peaks.broadPeak", stringsAsFactors = FALSE)
-totalpeaks<-totalpeaks[order(totalpeaks[,1], totalpeaks[,2]),]
-totalRegions.atac<-bed2index(totalpeaks[,1:3])
+	for (peak in seq_along(list_of_peaks)) {
+		# Sorts by chromosome and start position in case the peak calls are
+		# not already in this format. Sorting speeds up bedr functions.
+		peaks <- read.table(file_paths[peak], stringsAsFactors = FALSE)
+		peaks <- peaks[order(peaks[, 1], peaks[, 2], )]
 
-neunpeaks<-read.table("ATACSeq/CalledPeaks/AllData/OCT2020/N_peaks.broadPeak", stringsAsFactors = FALSE)
-neunpeaks<-neunpeaks[order(neunpeaks[,1], neunpeaks[,2]),]
-neunRegions.atac<-bed2index(neunpeaks[,1:3])
-
-soxpeaks<-read.table("ATACSeq/CalledPeaks/AllData/OCT2020/S_peaks.broadPeak", stringsAsFactors = FALSE)
-soxpeaks<-soxpeaks[order(soxpeaks[,1], soxpeaks[,2]),]
-soxRegions.atac<-bed2index(soxpeaks[,1:3])
-
-
-### load 5hmC peaks
-
-allpeaks<-read.table("CEGX/calledPeaks/All_peaks.narrowPeak", stringsAsFactors = FALSE)
-allpeaks<-allpeaks[order(allpeaks[,1], allpeaks[,2]),]
-allRegions.5hmc<-bed2index(allpeaks[,1:3])
-
-totalpeaks<-read.table("CEGX/calledPeaks/Total_peaks.narrowPeak", stringsAsFactors = FALSE)
-totalpeaks<-totalpeaks[order(totalpeaks[,1], totalpeaks[,2]),]
-totalRegions.5hmc<-bed2index(totalpeaks[,1:3])
-
-neunpeaks<-read.table("CEGX/calledPeaks/NeuN_peaks.narrowPeak", stringsAsFactors = FALSE)
-neunpeaks<-neunpeaks[order(neunpeaks[,1], neunpeaks[,2]),]
-neunRegions.5hmc<-bed2index(neunpeaks[,1:3])
-
-soxpeaks<-read.table("CEGX/calledPeaks/Sox10_peaks.narrowPeak", stringsAsFactors = FALSE)
-soxpeaks<-soxpeaks[order(soxpeaks[,1], soxpeaks[,2]),]
-soxRegions.5hmc<-bed2index(soxpeaks[,1:3])
-
-dnegpeaks<-read.table("CEGX/calledPeaks/DoubleNegative_peaks.narrowPeak", stringsAsFactors = FALSE)
-dnegpeaks<-dnegpeaks[order(dnegpeaks[,1], dnegpeaks[,2]),]
-dnegRegions.5hmc<-bed2index(dnegpeaks[,1:3])
-
-
-## create a background list of all peaks across all peak sets
-allReg<-bedr.merge.region(bedr.sort.region(c(allRegions.atac, totalRegions.atac, neunRegions.atac, soxRegions.atac, allRegions.5hmc, totalRegions.5hmc, neunRegions.5hmc, soxRegions.5hmc,dnegRegions.5hmc)))
-
-for(chr in 1:22){
-	annot<-read.table(gzfile(paste("/gpfs/mrc0/projects/Research_Project-MRC190311/References/LDScore/resources/grch38/baselineLD_v2.2/baselineLD.", chr,".annot.gz", sep = "")),header = TRUE)
-	snps<-fread(paste("/gpfs/mrc0/projects/Research_Project-MRC190311/References/LDScore/resources/grch38/plink_files/1000G.EUR.hg38.", chr,".bim", sep = ""))
-
-	## to create a single annotation of SNPs in baseline annotation filter to binary annotations
-	annot<-select_if(annot, is.integer)
-	
-	## extract SNP info
-	snpBed<-annot[,c("CHR", "BP", "BP")]
-	snpBed<-snpBed[order(snpBed$CHR, snpBed$BP),]
-	colnames(snpBed)<-c("chr", "start", "end")
-	snpBed$end<-snpBed$end+1
-	snpBed$chr<-paste("chr", snpBed$chr, sep = "")
-	class(snpBed$end)<-"integer"
-	snpRegions<-bed2index(as.data.frame(snpBed))
-	
-	## classify SNPs in atac peaks
-	atacALL<-in.region(snpRegions, allRegions.atac)
-	atacTOTAL<-in.region(snpRegions, totalRegions.atac)
-	atacNEUN<-in.region(snpRegions, neunRegions.atac)
-	atacSOX<-in.region(snpRegions, soxRegions.atac)
-	hmcALL<-in.region(snpRegions, allRegions.5hmc)
-	hmcTOTAL<-in.region(snpRegions, totalRegions.5hmc)
-	hmcNEUN<-in.region(snpRegions, neunRegions.5hmc)
-	hmcSOX<-in.region(snpRegions, soxRegions.5hmc)
-	hmcDNEG<-in.region(snpRegions, dnegRegions.5hmc)
-	overlapany<-in.region(snpRegions, allReg)
-	mergeOverlaps<-cbind(atacALL,atacTOTAL,atacNEUN, atacSOX, hmcALL, hmcTOTAL, hmcNEUN, hmcSOX, hmcDNEG)
-	## createcolumn of overlap with other regulatory features but not in any atac peak
-	outsidepeaks<-as.numeric(rowSums(annot[,-c(1:3)]) > 0)
-		
-	annOut<-data.frame(snps$V1, snps$V4, snps$V2, snps$V3, mergeOverlaps, outsidepeaks, annot$base)
-	colnames(annOut)<-c("CHR", "BP", "SNP", "CM","All_ATAC", "Total_ATAC", "NeuN_ATAC", "Sox10_ATAC", "All_5hmc", "Total_5hmc", "NeuN_5hmc", "Sox10_5hmc","DoubleNeg_5hmc", "AnyRegFeature", "AllSNPs")
-	mode(annOut$All_ATAC)<-"numeric"
-	mode(annOut$Total_ATAC)<-"numeric"
-	mode(annOut$NeuN_ATAC)<-"numeric"
-	mode(annOut$Sox10_ATAC)<-"numeric"
-	mode(annOut$All_5hmc)<-"numeric"
-	mode(annOut$Total_5hmc)<-"numeric"
-	mode(annOut$NeuN_5hmc)<-"numeric"
-	mode(annOut$Sox10_5hmc)<-"numeric"
-	mode(annOut$DoubleNeg_5hmc)<-"numeric"
-
-	write.table(annOut, paste("LDScoreRegression/NeuralAnnotations/NeuralCellRegulatoryPeaks.", chr, ".annot", sep = ""), quote = FALSE, row.names = FALSE)
+		regions <- bed2index(peaks[, 1:3])
+		sorted_peaks[[peak]] <- regions
+	}
+	return(sorted_peaks)
 }
+
+sorted_peaks <- read_peaks(peaks_file_paths)
+
+## This bedr function is expecting one bed file, so we bind all of the peak
+## files into one long bed file. The sort is for faster bedr processing
+## for in.region.
+## Commented out as not currently in use.
+# all_regions <- bedr.merge.region(bedr.sort.region(bind_rows(sorted_peaks)))
+
+for(chr in 1:22) {
+	annot <- read.table(gzfile(paste0(baseline_file_prefix, chr, ".annot.gz")), header = TRUE)
+	snps<-fread(paste0(bim_file_prefix, chr, ".bim"))
+
+	## This is in place to remove any annotation columns that are irrelevant
+	## or non-binary. Binary columns are required later for determining if a 
+	## snp resides in any of the baseline regulatory regions.
+	annot < -annot %>% select_if(function(x) all(is.integer(x) | x %in% c(0,1)))
+	
+	## We extract the base pair position twice to get a start
+	## and end read for each SNP (SNPs are one base pair always, so we only need
+	## the start BP and add one on to this value to get the end).
+	snp_bed<-annot[,c("CHR", "BP", "BP")]
+	snp_bed<-snp_bed[order(snp_bed$CHR, snp_bed$BP),]
+	colnames(snp_bed)<-c("chr", "start", "end")
+	snp_bed$end<-snp_bed$end+1
+	snp_bed$chr<-paste("chr", snp_bed$chr, sep = "")
+	class(snp_bed$end)<-"integer"
+	snp_regions<-bed2index(as.data.frame(snp_bed))
+	
+	## classify SNPs in the called peaks
+	snp_classifications <-
+	  lapply(sorted_peaks, function(peaks) in.region(snp_regions, peaks))
+	
+	## This is unused in the output and so is commented out. Adding this in
+	## is liable to introduce colinearity between annoation columns
+	# overlapany <- in.region(snpRegions, all_regions)
+
+	mergeOverlaps<-bind_cols(snp_classifications)
+
+	## Creates a column for snps that are in any feature given in the baseline
+	## annotation files.
+	## We exclude columns one to three as these columns are always positive
+	## (chromosome, position and base).  
+	any_regulatory_feature <- as.numeric(rowSums(annot[,-c(1:3)]) > 0)
+
+	output_annotation <- data.frame(snps$V1, snps$V4, snps$V2, snps$V3, mergeOverlaps, any_regulatory_feature, annot$base)
+	colnames(output_annotation) <-
+	  c("CHR", "BP", "SNP", "CM", peaks_names, "AnyRegulatoryFeature", "AllSNPs")
+
+	## This is to ensure R does not add quotes around integers, which would 
+	## result in errors once parsed into ldsc
+	output_annotation[peaks_names] <-
+	  lapply(output_annotation[peaks_names], as.numeric)
+
+	annotation_file_name <-
+	  paste0(ld_annotation_dir, "/", ld_annotation_prefix, ".", chr, ".annot.gz")
+
+	write.table(output_annotation, annotation_file_name, quote = FALSE, row.names = FALSE)
+}gg
