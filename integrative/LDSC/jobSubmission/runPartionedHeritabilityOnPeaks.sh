847078aff9fbb055339ab69681d937354113ac65
sof202 <s.o.fletcher@exeter.ac.uk>
2024-02-29 13:22:46 +0000

    Fix possible globbing errors

--- integrative/LDSC/jobSubmission/runPartionedHeritabilityOnPeaks.sh
+++ integrative/LDSC/jobSubmission/runPartionedHeritabilityOnPeaks.sh
@@ -15,28 +15,28 @@
 ## Compute LD scores with custom annotation file.
 for chr in {1..22};
 do
-  python ${LD_SOFTWARE_DIR}/ldsc.py \
+  python "${LD_SOFTWARE_DIR}/ldsc.py" \
 	--l2 \
-	--bfile ${LD_REFERENCE_DIR}/plink_files/${REFERENCE_PREFIX}.${chr} \
+	--bfile "${LD_REFERENCE_DIR}/plink_files/${REFERENCE_PREFIX}.${chr}" \
   --ld-wind-cm 1 \
-  --annot ${LD_ANNOTATION_DIR}/${ANNOTATION_PREFIX}.${chr}.annot \
-  --out ${LD_ANNOTATION_DIR}/${ANNOTATION_PREFIX}/${ANNOTATION_PREFIX}.${chr} \
-  --print-snps ${SNP_LISTS_DIR}/${SNP_LIST_PREFIX}.${chr}.snp
+  --annot "${LD_ANNOTATION_DIR}/${ANNOTATION_PREFIX}.${chr}.annot" \
+  --out "${LD_ANNOTATION_DIR}/${ANNOTATION_PREFIX}/${ANNOTATION_PREFIX}.${chr}" \
+  --print-snps "${SNP_LISTS_DIR}/${SNP_LIST_PREFIX}.${chr}.snp"
 done
 
 ## estimate partioned heritability for a range of GWAS traits
 gwastraits=($ls ${LD_GWAS_TRAITS_DIR}/*${GWAS_PATTERN}*.gz)
 
-for filename in ${gwastraits[@]}; 
+for filename in "${gwastraits[@]}"; 
 do
-  outfile=$(basename $filename .sumstats.gz)
+  outfile=$(basename "${filename}" .sumstats.gz)
 
-	python ${LD_SOFTWARE_DIR}/ldsc.py \
-	--h2 $filename \
-	--ref-ld-chr ${LD_ANNOTATION_DIR}/${ANNOTATION_PREFIX}/${ANNOTATION_PREFIX}. \
+	python "${LD_SOFTWARE_DIR}/ldsc.py" \
+	--h2 "${filename}" \
+	--ref-ld-chr "${LD_ANNOTATION_DIR}/${ANNOTATION_PREFIX}/${ANNOTATION_PREFIX}." \
 	--overlap-annot \
-	--frqfile-chr ${LD_REFERENCE_DIR}/frq_files/${REFERENCE_PREFIX}. \
-	--w-ld-chr  ${LD_REFERENCE_DIR}/weights/${WEIGHTS_PREFIX}. \
-	--out ${OUTPUTS_DIR}/${ANNOTATION_PREFIX}/${outfile} \
+	--frqfile-chr "${LD_REFERENCE_DIR}/frq_files/${REFERENCE_PREFIX}." \
+	--w-ld-chr  "${LD_REFERENCE_DIR}/weights/${WEIGHTS_PREFIX}." \
+	--out "${OUTPUTS_DIR}/${ANNOTATION_PREFIX}/${outfile}" \
 	--print-coefficients
 done
