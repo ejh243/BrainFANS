6131ad4ca919bbd17f04fbdef6a45252bc37d4cd
sof202 <s.o.fletcher@exeter.ac.uk>
2024-02-29 13:25:14 +0000

    prettier argument parsing

--- integrative/LDSC/jobSubmission/runPartionedHeritabilityOnPeaks.sh
+++ integrative/LDSC/jobSubmission/runPartionedHeritabilityOnPeaks.sh
@@ -15,12 +15,13 @@
 ## Compute LD scores with custom annotation file.
 for chr in {1..22};
 do
-  python "${LD_SOFTWARE_DIR}/ldsc.py" \
+  python \
+	"${LD_SOFTWARE_DIR}/ldsc.py" \
 	--l2 \
-	--bfile "${LD_REFERENCE_DIR}/plink_files/${REFERENCE_PREFIX}.${chr}" \
+	--bfile      "${LD_REFERENCE_DIR}/plink_files/${REFERENCE_PREFIX}.${chr}" \
   --ld-wind-cm 1 \
-  --annot "${LD_ANNOTATION_DIR}/${ANNOTATION_PREFIX}.${chr}.annot" \
-  --out "${LD_ANNOTATION_DIR}/${ANNOTATION_PREFIX}/${ANNOTATION_PREFIX}.${chr}" \
+  --annot      "${LD_ANNOTATION_DIR}/${ANNOTATION_PREFIX}.${chr}.annot" \
+  --out        "${LD_ANNOTATION_DIR}/${ANNOTATION_PREFIX}/${ANNOTATION_PREFIX}.${chr}" \
   --print-snps "${SNP_LISTS_DIR}/${SNP_LIST_PREFIX}.${chr}.snp"
 done
 
@@ -31,12 +32,13 @@ for filename in "${gwastraits[@]}";
 do
   outfile=$(basename "${filename}" .sumstats.gz)
 
-	python "${LD_SOFTWARE_DIR}/ldsc.py" \
-	--h2 "${filename}" \
-	--ref-ld-chr "${LD_ANNOTATION_DIR}/${ANNOTATION_PREFIX}/${ANNOTATION_PREFIX}." \
-	--overlap-annot \
+	python \
+	"${LD_SOFTWARE_DIR}/ldsc.py" \
+	--h2          "${filename}" \
+	--ref-ld-chr  "${LD_ANNOTATION_DIR}/${ANNOTATION_PREFIX}/${ANNOTATION_PREFIX}." \
 	--frqfile-chr "${LD_REFERENCE_DIR}/frq_files/${REFERENCE_PREFIX}." \
-	--w-ld-chr  "${LD_REFERENCE_DIR}/weights/${WEIGHTS_PREFIX}." \
-	--out "${OUTPUTS_DIR}/${ANNOTATION_PREFIX}/${outfile}" \
+	--w-ld-chr    "${LD_REFERENCE_DIR}/weights/${WEIGHTS_PREFIX}." \
+	--out         "${OUTPUTS_DIR}/${ANNOTATION_PREFIX}/${outfile}" \
+	--overlap-annot \
 	--print-coefficients
 done
