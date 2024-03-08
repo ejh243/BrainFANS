9160ea623ffe42c3b2ed2bb2c48afa3fd144a44d
sof202 <s.o.fletcher@exeter.ac.uk>
2024-03-08 12:40:46 +0000

    Fix crlf issues for the other file I didn't do in GitHub workshop

--- sequencing/DNAhydroxy/jobSubmission/peakCalling5hmCIncreasingSampleSizes.sh
+++ sequencing/DNAhydroxy/jobSubmission/peakCalling5hmCIncreasingSampleSizes.sh
@@ -1,61 +1,61 @@
-#!/bin/sh
-#PBS -V # export all environment variables to the batch job.
-#PBS -q mrcq # submit to the serial queue
-#PBS -l walltime=150:00:00 # Maximum wall time for the job.
-#PBS -A Research_Project-MRC190311 # research project to submit under. 
-#PBS -l procs=1 # specify number of processors.
-#PBS -m e -M e.j.hannon@exeter.ac.uk # email me at job completion
-#PBS -e LogFiles/IncSampleSizePeakCalling.err # error file
-#PBS -o LogFiles/IncSampleSizePeakCalling.log # output file
-
-## iteratively increase number of samples and redo peak calling
-
-module load MACS2
-
-ALIGNEDDIR=/gpfs/mrc0/projects/Research_Project-MRC190311/CEGX/alignedData
-PEAKDIR=/gpfs/mrc0/projects/Research_Project-MRC190311/CEGX/calledPeaks/IncreasingSampleSizes
-
-cd ${ALIGNEDDIR}
-
-SAMPLES=(CEG99-708-09 CEG99-708-21 CEG99-708-44 CEG99-708-58 CEG99-708-54 CEG99-708-59)
-
-# find pulldown and control aligned files
-PULLDOWNS=()
-CONTROLS=()
-for sampleName in ${SAMPLES[@]}
-do
-	CONTROLS+=($(ls ${sampleName}*IC*.GRCh38.karyo.deduplicated.bam))
-	PULLDOWNS+=($(ls ${sampleName}*PC*.GRCh38.karyo.deduplicated.bam))
-done
-
-for NSAMPLES in {2..6}
-do
-	macs2 callpeak -t ${PULLDOWNS[@]:0:${NSAMPLES}} -c ${CONTROLS[@]:0:${NSAMPLES}} -f BAM -g hs -n NeuN5hmCPeakCalling_${NSAMPLES}_Samples -B -q 0.01 --outdir ${PEAKDIR}
-done
-
-SAMPLES=(CEG99-708-20 CEG99-708-30 CEG99-708-53 CEG99-708-07 CEG99-708-08 CEG99-708-17)
-
-# find pulldown and control aligned files
-PULLDOWNS=()
-CONTROLS=()
-for sampleName in ${SAMPLES[@]}
-do
-	CONTROLS+=($(ls ${sampleName}*IC*.GRCh38.karyo.deduplicated.bam))
-	PULLDOWNS+=($(ls ${sampleName}*PC*.GRCh38.karyo.deduplicated.bam))
-done
-
-
-for NSAMPLES in {2..6}
-do
-	macs2 callpeak -t ${PULLDOWNS[@]:0:${NSAMPLES}} -c ${CONTROLS[@]:0:${NSAMPLES}} -f BAM -g hs -n Sox5hmCPeakCalling_${NSAMPLES}_Samples -B -q 0.01 --outdir ${PEAKDIR}
-done
-
-CONTROLS=($(ls *IC*.GRCh38.karyo.deduplicated.bam))
-PULLDOWNS=($(ls *PC*.GRCh38.karyo.deduplicated.bam))
-
-for NSAMPLES in {2..19}
-do
-	macs2 callpeak -t ${PULLDOWNS[@]:0:${NSAMPLES}} -c ${CONTROLS[@]:0:${NSAMPLES}} -f BAM -g hs -n All5hmCPeakCalling_${NSAMPLES}_Samples -B -q 0.01 --outdir ${PEAKDIR}
-done
-
-
+#!/bin/sh[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+#PBS -V # export all environment variables to the batch job.[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+#PBS -q mrcq # submit to the serial queue[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+#PBS -l walltime=150:00:00 # Maximum wall time for the job.[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+#PBS -A Research_Project-MRC190311 # research project to submit under. [DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+#PBS -l procs=1 # specify number of processors.[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+#PBS -m e -M e.j.hannon@exeter.ac.uk # email me at job completion[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+#PBS -e LogFiles/IncSampleSizePeakCalling.err # error file[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+#PBS -o LogFiles/IncSampleSizePeakCalling.log # output file[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+## iteratively increase number of samples and redo peak calling[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+module load MACS2[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+ALIGNEDDIR=/gpfs/mrc0/projects/Research_Project-MRC190311/CEGX/alignedData[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+PEAKDIR=/gpfs/mrc0/projects/Research_Project-MRC190311/CEGX/calledPeaks/IncreasingSampleSizes[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+cd ${ALIGNEDDIR}[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+SAMPLES=(CEG99-708-09 CEG99-708-21 CEG99-708-44 CEG99-708-58 CEG99-708-54 CEG99-708-59)[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+# find pulldown and control aligned files[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+PULLDOWNS=()[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+CONTROLS=()[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+for sampleName in ${SAMPLES[@]}[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+do[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+	CONTROLS+=($(ls ${sampleName}*IC*.GRCh38.karyo.deduplicated.bam))[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+	PULLDOWNS+=($(ls ${sampleName}*PC*.GRCh38.karyo.deduplicated.bam))[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+done[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+for NSAMPLES in {2..6}[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+do[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+	macs2 callpeak -t ${PULLDOWNS[@]:0:${NSAMPLES}} -c ${CONTROLS[@]:0:${NSAMPLES}} -f BAM -g hs -n NeuN5hmCPeakCalling_${NSAMPLES}_Samples -B -q 0.01 --outdir ${PEAKDIR}[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+done[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+SAMPLES=(CEG99-708-20 CEG99-708-30 CEG99-708-53 CEG99-708-07 CEG99-708-08 CEG99-708-17)[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+# find pulldown and control aligned files[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+PULLDOWNS=()[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+CONTROLS=()[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+for sampleName in ${SAMPLES[@]}[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+do[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+	CONTROLS+=($(ls ${sampleName}*IC*.GRCh38.karyo.deduplicated.bam))[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+	PULLDOWNS+=($(ls ${sampleName}*PC*.GRCh38.karyo.deduplicated.bam))[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+done[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+for NSAMPLES in {2..6}[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+do[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+	macs2 callpeak -t ${PULLDOWNS[@]:0:${NSAMPLES}} -c ${CONTROLS[@]:0:${NSAMPLES}} -f BAM -g hs -n Sox5hmCPeakCalling_${NSAMPLES}_Samples -B -q 0.01 --outdir ${PEAKDIR}[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+done[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+CONTROLS=($(ls *IC*.GRCh38.karyo.deduplicated.bam))[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+PULLDOWNS=($(ls *PC*.GRCh38.karyo.deduplicated.bam))[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+for NSAMPLES in {2..19}[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+do[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+	macs2 callpeak -t ${PULLDOWNS[@]:0:${NSAMPLES}} -c ${CONTROLS[@]:0:${NSAMPLES}} -f BAM -g hs -n All5hmCPeakCalling_${NSAMPLES}_Samples -B -q 0.01 --outdir ${PEAKDIR}[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+done[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
+[DO-NOT-ACCEPT-THIS-PULL-REQUEST]
