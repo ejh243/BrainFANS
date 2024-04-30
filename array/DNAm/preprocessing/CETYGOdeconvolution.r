0140b711e289bb345c2f7628acedf2d23f72eac1
ew367 <44901334+ew367@users.noreply.github.com>
2024-04-30 13:54:26 +0100

    Dnam qc epicv2 (with pre-QC-checks already merged) (#116)
    * test
    
    * test
    
    * Include R version and array type
    
    * uploading latest changes
    
    * testingPush
    
    * added path to config.r to define epicv2 or epicv1 data type
    
    * load in manifest file
    
    * fixed bug where bscon not added to QCmetrics for gds format data
    
    * edited PCA on beta vals to not need rawbetas obj
    
    * add in if statement for epicv1 or v2
    
    * edit DNAmAge calc to handle epicv2 array
    
    * edit to handle epicv2 data
    
    * edit to handle epicv2 data
    
    * edit count NA'sPer to not require separate rawbetas obj
    
    * edit predSex to handle epicv2 data (use manifest for chr info)
    
    * edit snpCor object work with gds file
    
    * changed sex pred thresholds
    
    * edit genocheck to handle epicv2 data
    
    * updated genoCheck for Epic v2
    
    * edited CorticalClock to extract betas from gds
    
    * edit sex pred thresholds
    
    * update remaining if statements for V2/V1 data check
    
    * Fixed reloading GDS bug, added EPICv2 annot
    
    * change sex pred thresholds for epicv1
    
    * edit to handle hm450k data
    
    * edit to load config files from project dir
    
    * edit to optioanlly load 450k manifest
    
    * edit pca auto betas to handle 450k data
    
    * update sex prediction to handle 450k data
    
    * add code to load manifest files for 450k/epicv2 data types
    
    * edit dataset up to use manifest file chr info for epicV2 and 450k data
    
    * edited to need refDir to run clusterCellTypes.r
    
    * add extra filepaths
    
    * add in load manifests for epicv2/450k data
    
    * edit to need refDir as command line arg
    
    * edit for 450k/epicv2 handling
    
    * added in manifest files for the different array types
    
    * added script to pipeline to run CETYGO
    
    * remove temp files
    
    * added missing data.table package load
    
    * add in missing data.table package load command
    
    * edit CETYGO script to handle epicv2 data
    
    * fixes #61
    
    * add CETYGO package installation
    
    * install dev versions of bigmelon and wateRmelon
    
    * add in libraries for rendering rmd QC report
    
    * move all library calls to beginning of script
    
    * change toupper(arrayType) to only be at beginning of script
    
    * add in function for blood tissue
    
    * edit to add option for bulk tissue
    
    * removed mset option
    
    * tidied up
    
    * change to only have tpupper call once at beginning of script and remove mset options
    
    * edit to only have toupper call once at start
    
    * removed mset option
    
    * edit to have toupper only at start
    
    * edit to include toupper only once at start
    
    * edit to include toupper only once at start
    
    * removed date and author info
    
    * removed date and author info
    
    * add dummy Cell_Type col for bulk data
    
    * change arrayVersion to arrayType
    
    * edited type in adultBloodCETYGO function
    
    * add blank line
    
    * Change permissions for raw.gds and rawNorm.gds
    
    * fix the missing GenotypeIID
    
    ---------
    
    Co-authored-by: alicemfr <franklinalicem@gmail.com>
    Co-authored-by: ejh243 <e.j.hannon@exeter.ac.uk>
    Co-authored-by: Siyi Wang <s.w.wang@exeter.ac.uk>

---
+++ array/DNAm/preprocessing/CETYGOdeconvolution.r
@@ -0,0 +1,235 @@
+##---------------------------------------------------------------------#
+##
+## Title: Run CETYGO cell type deconvolution
+##
+## Purpose of script: Use CETYGO brain ref panal or blood ref panal depending on ## tissue type to run CETYGO on the samples
+##
+##
+##---------------------------------------------------------------------#
+
+
+#----------------------------------------------------------------------#
+# NOTES
+#----------------------------------------------------------------------#
+# project folder is provided on command line
+# More info on the CETYGO package can be found here: 
+#   https://github.com/ejh243/CETYGO/wiki/Deconvolution-of-brain-cell-types
+
+
+#----------------------------------------------------------------------#
+# DEFINE PARAMETERS
+#----------------------------------------------------------------------#
+
+args<-commandArgs(trailingOnly = TRUE)
+dataDir <- args[1]
+
+gdsFile <-paste0(dataDir, "/2_gds/raw.gds")
+configFile <- paste0(dataDir, "/config.r")
+
+source(configFile)
+
+arrayType <- toupper(arrayType)
+tissueType <- toupper(tissueType)
+
+
+#----------------------------------------------------------------------#
+# LOAD PACKAGES
+#----------------------------------------------------------------------#
+
+library(CETYGO)
+library(gridExtra)
+library(bigmelon)
+library(ggplot2)
+library(wateRmelon)
+
+
+#----------------------------------------------------------------------#
+# IMPORT DATA
+#----------------------------------------------------------------------#
+
+setwd(dataDir)
+
+# load sample sheet
+sampleSheet<-read.csv("0_metadata/sampleSheet.csv", na.strings = c("", "NA"), stringsAsFactors = FALSE)
+# if no column Basename, creates from columns Chip.ID and Chip.Location
+if(!"Basename" %in% colnames(sampleSheet)){
+	sampleSheet$Basename<-paste(sampleSheet$Chip.ID, sampleSheet$Chip.Location, sep = "_")
+}
+
+
+
+gfile<-openfn.gds(gdsFile, readonly = FALSE, allow.fork = TRUE)
+# ensure sample sheet is in same order as data
+sampleSheet<-sampleSheet[match(colnames(gfile), sampleSheet$Basename),]
+# extract a few useful matrices
+if(arrayType == "V2"){
+	 rawbetas<-epicv2clean(betas(gfile)[])
+} else {
+	rawbetas<-betas(gfile)[,]
+}
+closefn.gds(gfile)
+
+
+
+#----------------------------------------------------------------------#
+# DEFINE CETYGO FUNCTION FOR BRAIN TISSUE
+#----------------------------------------------------------------------#
+
+adultBrainCETYGO <- function(betas, cellType){
+  
+  predPropAll<-list() # store output in list
+  CETYGOplots <- list() # store CETYGO score plots in list
+  propPlots <- list() # store proportion plots in list
+  counter = 1 # counter for storing list elements from for loop
+  maxCETYGO <- 0 # vector to hold max CETYGO score from each model/ref
+  minCETYGO <- 1 # vector to hold min CETYGO score from each model/ref
+  
+  # run CETYGO using both ANOVA and IDOL methods and all available cell ref panals 
+  for(method in names(modelBrainCoef)){
+    for(j in 1:length(modelBrainCoef[[method]])){
+      if(!is.null(modelBrainCoef[[method]][[j]])){
+        predPropAll[[method]][[j]]<-projectCellTypeWithError(betas, modelBrainCoef[[method]][[j]])
+      }
+    } 
+  }
+  
+  # Find max and min CETYGO scores across all methods/ref panals to calibrate axis limits
+  for(i in 1:length(predPropAll)){
+    type <- predPropAll[[i]]
+    for(j in 1:length(type)){
+      plotdf <- as.data.frame(type[[j]])
+      
+      if (ncol(plotdf) > 0) {
+        
+        if (max(plotdf$CETYGO) > maxCETYGO){
+          maxCETYGO <- max(plotdf$CETYGO)}
+        
+        if (min(plotdf$CETYGO) < minCETYGO){
+          minCETYGO <- min(plotdf$CETYGO)}
+      }
+    }
+  }
+  
+  # create boxplots of CETYGO scores and proportions
+  for(i in 1:length(predPropAll)){
+    type <- predPropAll[[i]]
+    for(j in 1:length(type)){
+      plotdf <- as.data.frame(type[[j]])
+      
+      if (ncol(plotdf) > 0) {
+        
+        # CETYGO score boxplot
+        pCETYGO <- ggplot(plotdf, aes(factor(0), CETYGO))+
+          geom_boxplot()+
+          coord_cartesian(ylim=c(minCETYGO - 0.01, maxCETYGO + 0.01))+
+          ggtitle(paste0(cellType, "_", names(predPropAll[i]), "-", j))+
+          theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank(), plot.title = element_text(size=6))+
+          geom_hline(yintercept=0.07, linetype="dashed", color = "red")
+        
+        CETYGOplots[[counter]] <- pCETYGO
+        
+        # proportions boxplot
+        plotdf <-as.data.frame(plotdf[,1:(ncol(plotdf)-2)])
+        plotdf$Basename <- rownames(plotdf)
+        plotdf <- reshape2::melt(plotdf, id.vars = "Basename")
+        colnames(plotdf) <- c("Basename", "CellType", "Proportion")
+        
+        p <- ggplot(plotdf, aes(x=CellType, y = Proportion)) +
+          geom_boxplot()+
+          ggtitle(paste0(cellType, "_", names(predPropAll[i]), "-", j))+
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+        
+        propPlots[[counter]] <- p
+        
+        counter = counter + 1
+      }
+    }
+  }
+  
+  # arrange plots into to single object and save all output
+  CETYGOplotFile <- paste0(dataDir, "/2_gds/QCmetrics/CETYGO/CETYGOscorePlots_", cellType, ".pdf")
+  pdf(CETYGOplotFile)
+  do.call(grid.arrange, c(CETYGOplots, ncol = 4))
+  dev.off()
+  
+  propPlotsFile <- paste0(dataDir, "/2_gds/QCmetrics/CETYGO/CETYGOcellPropsPlots_", cellType, ".pdf")
+  pdf(propPlotsFile, height = 20, width = 20)
+  do.call(grid.arrange, c(propPlots, ncol = 4))
+  dev.off()
+  
+  predPropOutFile <- paste0(dataDir, "/2_gds/QCmetrics/CETYGO/CETYGOpredictedProportions_", cellType, ".rdat")
+  save(predPropAll, file = predPropOutFile)
+  
+}
+
+
+#----------------------------------------------------------------------#
+# DEFINE CETYGO FUNCTION FOR BLOOD TISSUE
+#----------------------------------------------------------------------#
+
+
+adultBloodCETYGO <- function(betas){
+  
+  # run CETYGO using blood ref panal
+  rowIndex<-rownames(betas)[rownames(betas) %in% rownames(modelBloodCoef)]
+  predProp<-as.data.frame(projectCellTypeWithError(betas, modelBloodCoef[rowIndex,]))
+
+
+  # boxPlot CETYGO score
+  pCETYGO <- ggplot(predProp, aes(factor(0), CETYGO))+
+    geom_boxplot()+
+  geom_hline(yintercept=0.07, linetype="dashed", color = "red")+
+    xlab("")
+  
+  #save plot
+  CETYGOplotFile <- paste0(dataDir, "/2_gds/QCmetrics/CETYGO/CETYGOscorePlot.pdf")
+  pdf(CETYGOplotFile)
+  print(pCETYGO)
+  dev.off()
+
+  
+  # box plot proportions
+  plotdf <-as.data.frame(predProp[,1:(ncol(predProp)-2)])
+  plotdf$Basename <- rownames(plotdf)
+  plotdf <- reshape2::melt(plotdf, id.vars = "Basename")
+  colnames(plotdf) <- c("Basename", "CellType", "Proportion")
+
+  p <- ggplot(plotdf, aes(x=CellType, y=Proportion)) +
+    geom_boxplot()+
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+  
+  # save plot
+  propPlotsFile <- paste0(dataDir, "/2_gds/QCmetrics/CETYGO/CETYGOcellPropsPlot.pdf")
+  pdf(propPlotsFile)
+  print(p)
+  dev.off()
+  
+  # save output file
+  predPropOutFile <- paste0(dataDir, "/2_gds/QCmetrics/CETYGO/CETYGOpredictedProportions.rdat")
+  save(predProp, file = predPropOutFile)
+
+}
+
+
+
+#----------------------------------------------------------------------#
+# RUN CETYGO FUNCTION 
+#----------------------------------------------------------------------#
+
+# for sorted Brain tissue run on each cell type individually
+if(tissueType == "BRAIN" & cellSorted == "TRUE"){
+
+  for(cell in sampleSheet$Cell_Type){
+      cellSampleSheet <- sampleSheet[which(sampleSheet$Cell_Type == cell),]
+      cellBetas <- rawbetas[, colnames(rawbetas) %in% cellSampleSheet$Basename]
+      adultBrainCETYGO(cellBetas, cell)
+  }
+} else {
+  if(tissueType == "BLOOD"){
+    adultBloodCETYGO(rawbetas)
+  } else {
+    if(tissueType == "BRAIN" & cellSorted == "FALSE"){
+      adultBrainCETYGO(rawbetas, "bulk")
+    }
+  }
+}
