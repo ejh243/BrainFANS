a0421330bcaf8385a661b60f68b6526c818ef646
alicemfr <franklinalicem@gmail.com>
2024-05-15 18:32:32 +0100

    Add option for absence of Basename column

--- array/DNAm/preprocessing/checkColnamesSampleSheet.r
+++ array/DNAm/preprocessing/checkColnamesSampleSheet.r
@@ -15,9 +15,10 @@ library(stringdist) # for amatch()
 sampleSheet <- read.csv(paste0(dataDir, "/0_metadata/sampleSheet.csv"), na.strings = c("", "NA"), stringsAsFactors = FALSE)
 
 # Column names to test
-req_cols <- c('Sample_ID','Individual_ID','Chip_ID','Chip_Location') 	# required column names
-opt_cols <- c('Age')													# optional column names
-cond_cols <- c('Sex','Genotype_IID','Cell_Type')						# conditionally column names
+req_cols <- c('Sample_ID','Individual_ID') 												# required column names
+bsnm_cols <- c('Basename','Chip_ID','Chip_Location','Sentrix_ID','Sentrix_Position')	# required when Basename not present
+opt_cols <- c('Age')																	# optional column names
+cond_cols <- c('Sex','Genotype_IID','Cell_Type')										# conditionally column names
 
 # source checkColnames()
 source("checkColnamesFunction.r")
@@ -25,8 +26,32 @@ source("checkColnamesFunction.r")
 
 #1. Check required column names ------------------------------------------------------------------------------
 print("1. Checking required column names:")
-print(req_cols)
-checkColnames(sampleSheet, req_cols, type='Required')
+print(c('Basename',req_cols))
+
+# check Basename first
+b <- checkColnames(sampleSheet, bsnm_cols[1], type='Required', verbose=F)
+
+if(b$allPresent){
+	# if Basename present, continue to check Basename alongside other required columns
+	checkColnames(sampleSheet, c('Basename',req_cols), type='Required')
+}else{
+	# if Basename not present, check Chip and Sentrix as alternatives
+	chip <- checkColnames(sampleSheet, bsnm_cols[2:3], type='Required', verbose=F)
+	sntrx <- checkColnames(sampleSheet, bsnm_cols[4:5], type='Required', verbose=F)
+	
+	# if either Chip or Sentrix present, continue to check other required columns
+	if(any(chip$allPresent | sntrx$allPresent)){
+		cat("Basename column not found, but at least 2 of the following alternative columns are present: ", bsnm_cols[2:5])
+		cat("Checking remaining required columns")
+		checkColnames(sampleSheet, req_cols, type='Required')
+	}else{
+		cat(c("Basename column not found, and neither set of alternative column names are present: ", bsnm_cols[2:5]))
+		cat("Checking remaining required columns")
+		checkColnames(sampleSheet, req_cols, type='Required')
+	}
+}
+
+
 
 
 #2. Check optional column names ------------------------------------------------------------------------------
