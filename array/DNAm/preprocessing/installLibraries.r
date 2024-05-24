a3e46788b6f282d464bedc9d76207b5319edd63d
alicemfr <franklinalicem@gmail.com>
2024-05-24 14:02:53 +0100

    Check all packages installed

--- array/DNAm/preprocessing/installLibraries.r
+++ array/DNAm/preprocessing/installLibraries.r
@@ -10,6 +10,8 @@
 ##
 ##---------------------------------------------------------------------#
 
+'%ni%' <- Negate('%in%')
+
 args <- commandArgs(trailingOnly = TRUE)
 dataDir <- args[1]
 
@@ -51,21 +53,45 @@ if(arrayType=='V2'){
 #---------------------------------------------------------------------#
 # INSTALL PACKAGES FOR QC METRICS
 #---------------------------------------------------------------------#
-
-install.packages(setdiff(c("e1071","stringdist","data.table"), rownames(installed.packages())),repos = "https://cran.r-project.org")
+pkgs_qc <- c("e1071","stringdist","data.table")
+install.packages(setdiff(pkgs_qc, rownames(installed.packages())),repos = "https://cran.r-project.org")
 
 #---------------------------------------------------------------------#
 # INSTALL PACKAGES FOR BRAIN CELL PROPORTION PREDICTION
 #---------------------------------------------------------------------#
 #Creating QC reports
-install.packages(setdiff(c("pander","kableExtra"), rownames(installed.packages())),repos = "https://cran.r-project.org")
+pkgs_rep <- c("pander","kableExtra")
+install.packages(setdiff(pkgs_rep, rownames(installed.packages())),repos = "https://cran.r-project.org")
 
 #Additional packages for Brain Cell Proportion Prediction
 BiocManager::install(c("genefilter", "minfi"))
-install.packages(setdiff("quadprog", rownames(installed.packages())),repos = "https://cran.r-project.org")
+pkgs_pred <- c("devtools","quadprog")
+install.packages(setdiff(pkgs_pred, rownames(installed.packages())),repos = "https://cran.r-project.org")
 
-# install devtools to install from GitHub
-install.packages(setdiff("devtools", rownames(installed.packages())),repos = "https://cran.r-project.org")
+# load devtools to install from GitHub
 library(devtools)
 install_github("ds420/CETYGO")
 install_github("EpigeneticsExeter/cdegUtilities")
+
+
+## Check all packages installed successfully ##
+all_pkgs <- c("wateRmelon", "bigmelon", pkgs_qc, pkgs_rep, "genefilter", "minfi", pkgs_pred, "CETYGO", "cdegUtilities")
+if(arrayType=='450K'){
+  all_pkgs <- c(all_pkgs, "IlluminaHumanMethylation450kanno.ilmn12.hg19", "IlluminaHumanMethylation450kmanifest")
+}
+if(arrayType=='V1'){
+  all_pkgs <- c(all_pkgs, "IlluminaHumanMethylationEPICanno.ilm10b2.hg19", "IlluminaHumanMethylationEPICmanifest")
+}
+if(arrayType=='V2'){
+  all_pkgs <- c(all_pkgs, "IlluminaHumanMethylationEPICv2anno.20a1.hg38", "IlluminaHumanMethylationEPICv2manifest")
+}
+if(all(all_pkgs %in% rownames(installed.packages()))){
+  print("All packages successfully installed")
+}else{
+  absent <- all_pkgs[all_pkgs %ni% rownames(installed.packages())]
+  print(paste("Failed installation of", length(absent), "packages:", absent))
+}
+
+
+
+
