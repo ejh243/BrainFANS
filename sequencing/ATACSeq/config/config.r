edc3566994dbe1f18897c81f3e9abe87e1cb3449
marinafloresp <m.flores-payan@exeter.ac.uk>
2024-05-31 14:38:13 +0100

    Add example config files for running pipeline

---
+++ sequencing/ATACSeq/config/config.r
@@ -0,0 +1,41 @@
+## ===========================================================##
+##           ATAC-seq pipeline: Example config R file         ##
+## ===========================================================##
+##                                                            ||
+## This is an example config R file for the ATAC-sequencing   ||
+## pipeline. This is expected to be located in the project's  ||
+## directory. The full path to this file should be specified  ||
+## in the main config file.                                   ||
+##                                                            ||
+## The project's directory should be specified in the "dir"   ||
+## variable.                                                  ||
+##                                                            ||
+## ===========================================================##
+
+##Need to specify directory to project folder.
+dir<- "/lustre/projects/Research_Project-MRC190311/ATACSeq/<project>"
+
+metaDir<- paste0(dir, "/0_metadata")
+dataDir<-paste0(dir, "/1_raw")
+fastQCDir<-paste0(dir, "/1_raw/fastqc")
+trimDir<-paste0(dir, "/2_trimmed") 
+alignedDir<-paste0(dir, "/3_aligned")
+peakDir<- paste0(dir, "/4_calledPeaks")
+qcDir<-paste0(alignedDir, "/QCOutput" )
+sampleSheet<-paste0(metaDir, "/sampleSheet.csv")
+analysisDir<-paste0(dir, "/6_analysis")
+
+## create colourblind friendly palette
+colorBlindGrey8   <- c("#009E73", "#CC79A7", "#D55E00", "#999999", 
+                       "#F0E442", "#0072B2",  "#E69F00", "#56B4E9")
+
+nrfThres <- 0.7
+nucfThres <- 0.3
+monoThres <- 0.2
+dipPThres <-0.5
+alignThres <- 0.8
+uniqReads <- 2
+pbc1Thres <- 0.7
+pbc2Thres <- 1
+peakThres <- 0
+fripThres <- 0
