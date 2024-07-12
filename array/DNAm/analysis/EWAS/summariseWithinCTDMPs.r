569aea64c7ef8c1ddf889e835f638a4d8f237398
Eilis Hannon <e.j.hannon@exeter.ac.uk>
2024-07-12 17:07:10 +0100

    add more plots

--- array/DNAm/analysis/EWAS/summariseWithinCTDMPs.r
+++ array/DNAm/analysis/EWAS/summariseWithinCTDMPs.r
@@ -6,8 +6,9 @@
 ##
 ## ---------------------------------------------------------------------#
 
-library(qqman)
-
+#----------------------------------------------------------------------#
+# DEFINE PARAMETERS
+#----------------------------------------------------------------------#
 args <- commandArgs(trailingOnly = TRUE)
 dataDir <- args[1]
 refPath <- args[2]
@@ -16,6 +17,17 @@ normData <- file.path(dataDir, "3_normalised/normalised.rdata")
 resPath <- file.path(dataDir, "4_analysis/EWAS")
 
 cellTypes <- c("Double-", "NeuN+", "Sox10+")
+
+#----------------------------------------------------------------------#
+# LOAD PACKAGES
+#----------------------------------------------------------------------#
+library(qqman)
+library(tidyr)
+library(ggplot2)
+
+#----------------------------------------------------------------------#
+# IMPORT DATA
+#----------------------------------------------------------------------#
 
 res<-list()
 for (each in cellTypes) {
@@ -73,50 +85,60 @@ for(i in 1:3){
 
 ## QQ plots
 
-tiff(file.path(resPath, "Plots","QQplotsWithinCTLRSCZEffectNoCCNoCETYGO.tiff"), width = 1200, height = 400)
-par(mfrow = c(1,3))
+png(file.path(resPath, "Plots","QQplotsWithinCTLRSCZEffect.png"), width = 1200, height = 1200, res = 200)
+par(mfrow = c(3,3))
+par(mar = c(4,4,1,1))
+par(mgp = c(2,0.75,0))
 for(i in 1:3){
     qq(res[[i]][,"NullModel_SCZ_P"], main = cellTypes[i])
 }
-dev.off()
-
-tiff(file.path(resPath, "Plots","QQplotsWithinCTLRSCZEffectNoCCWithCETYGO.tiff"), width = 1200, height = 400)
-par(mfrow = c(1,3))
+for(i in 1:3){
+    if("CCModel_SCZ_P" %in% colnames(res[[i]])){
+        qq(res[[i]][,"CCModel_SCZ_P"], main = cellTypes[i])
+    } else {
+        plot(0, xaxt = 'n', yaxt = 'n', bty = 'n', pch = '', ylab = '', xlab = '')
+    }
+}
 for(i in 1:3){
     qq(res[[i]][,"FullModel_SCZ_P"], main = cellTypes[i])
 }
+
 dev.off()
 
 ## Compare with and without CETYGO & cell composition
-
+png(file.path(resPath, "Plots","ScatterplotsWithinCTLRSCZEffect.png"), width = 800, height = 1200, res = 200)
 par(mfrow = c(3,2))
+par(mar = c(4,4,1,1))
+par(mgp = c(2,0.75,0))
 for(i in 1:3){
     if("CCModel_SCZ_P" %in% colnames(res[[i]])){
         plot(-log10(res[[i]][,"FullModel_SCZ_P"]), -log10(res[[i]][,"CCModel_SCZ_P"]), pch = 16, 
-        xlab = "With CETYGO", ylab = "Without CETYGO", main = cellTypes[i])
+        xlab = "With CETYGO", ylab = "Without CETYGO", main = cellTypes[i], col = "darkgrey")
         abline(a = 0, b = 1)
         plot(-log10(res[[i]][,"CCModel_SCZ_P"]), -log10(res[[i]][,"NullModel_SCZ_P"]), pch = 16, 
-        xlab = "With cell composition", ylab = "Without cell composition", main = cellTypes[i])
+        xlab = "With cell composition", ylab = "Without cell composition", main = cellTypes[i], col = "darkgrey")
         abline(a = 0, b = 1)
     } else {
         plot(-log10(res[[i]][,"FullModel_SCZ_P"]), -log10(res[[i]][,"NullModel_SCZ_P"]), pch = 16, 
-        xlab = "With CETYGO", ylab = "Without CETYGO", main = cellTypes[i])
+        xlab = "With CETYGO", ylab = "Without CETYGO", main = cellTypes[i], col = "darkgrey")
         abline(a = 0, b = 1)
     }
 }
+dev.off()
 
 # Manhattan plots
 
-jpeg(file.path(resPath, "Plots", "ManhattanPlotsWithinCTLM.jpeg"), width = 600, height = 600, quality = 100)
+png(file.path(resPath, "Plots", "ManhattanPlotsWithinCTLM.png"), width = 1400, height = 1000, res = 200)
 par(mfrow = c(3,1))
 par(mar = c(4,4,0.5,0.5))
+par(mgp = c(2,0.75,0))
 for(i in 1:3){
     res[[i]][,"chrm"][which(res[[i]][,"chrm"] == "X")]<-"23"
     res[[i]][,"chrm"]<-as.numeric(as.character(res[[i]][,"chrm"]))
 
     resMan<-data.frame("SNP" = rownames(res[[i]]), "P" = res[[i]][,"FullModel_SCZ_P"], "CHR" = res[[i]][,"chrm"],"BP" = res[[i]][,"start"])
     resMan<-na.omit(resMan)
-    manhattan(resMan, genomewideline = -log10(9e-8), suggestiveline = -log10(5e-5), main = cellTypes[i])
+    manhattan(resMan, genomewideline = -log10(9e-8), suggestiveline = -log10(1e-5), main = cellTypes[i])
 } 
 dev.off()
 
@@ -141,4 +163,110 @@ for(thres in c(9e-8, 1e-7, 1e-6, 1e-5)){
 }
 cbind()
 
-write.csv(nDMPs, file.path(resPath, "DMPCountsPerCelltypeAcrossWithinCTModels.csv"))
+write.csv(nDMPs, file.path(resPath, "Tables", "DMPCountsPerCelltypeAcrossWithinCTModels.csv"))
+
+## COMPARE DMPS ACROSS CELL TYPES
+thres<-1e-5
+
+dmpList<-NULL
+for(i in 1:3){
+    dmpList<-rbind(dmpList, cbind(rownames(res[[i]])[which(res[[i]][,"FullModel_SCZ_P"] < thres)], cellTypes[i]))
+}
+colnames(dmpList)<-c("ProbeID", "DiscoveryCellType")
+
+
+
+dmpRes<-cbind(dmpList, res[[1]][dmpList[,1], c("FullModel_SCZ_P", "FullModel_SCZ_coeff", "FullModel_SCZ_SE")],
+res[[2]][dmpList[,1], c("FullModel_SCZ_P", "FullModel_SCZ_coeff", "FullModel_SCZ_SE")],
+res[[3]][dmpList[,1], c("FullModel_SCZ_P", "FullModel_SCZ_coeff", "FullModel_SCZ_SE")])
+colnames(dmpRes)<-c("ProbeID", "DiscoveryCellType", outer(c("P", "Coeff", "SE"), cellTypes, paste, sep = ":"))
+
+
+
+diffLong<-pivot_longer(data.frame(dmpRes[,c(2,4,7,10)]), cols = gsub("\\+|-", "\\.", paste("Coeff", cellTypes, sep = ".")))
+diffLong$name<-gsub("Coeff\\.", "", diffLong$name)
+diffLong$value<-abs(diffLong$value)
+pdf(file.path(resPath, "Plots","ViolinPlotCelltypeEffectsDiscoveryDMPsLMWithinCTs.pdf"), width = 8, height = 4)
+ggplot(diffLong, aes(x = name, y = value, fill = name)) + geom_violin() +
+xlab("Cell Type") + ylab("Mean difference") + 
+stat_summary(fun=mean, geom="point", size=2, color="black") + facet_wrap (~DiscoveryCellType)
+dev.off()
+
+plotLim<-range(dmpRes[,c(4,7,10)])
+
+p1<- subset(dmpRes, DiscoveryCellType == "NeuN+") %>%
+ggplot(aes(x = `Coeff:Double-`, y = `Coeff:NeuN+`)) + geom_point() +
+xlab("Double -ve") + ylab("NeuN +ve") + ggtitle("NeuN+ve DMPs") +
+xlim(plotLim) + ylim(plotLim) +
+geom_hline(yintercept = 0) + 
+geom_vline(xintercept = 0)
+
+p2<- subset(dmpRes, DiscoveryCellType == "NeuN+") %>%
+ggplot(aes(x = `Coeff:Sox10+`, y = `Coeff:NeuN+`)) + geom_point() +
+xlab("Sox10 +ve") + ylab("NeuN +ve") + ggtitle("NeuN+ve DMPs") +
+xlim(plotLim) + ylim(plotLim) +
+geom_hline(yintercept = 0) + 
+geom_vline(xintercept = 0)
+
+p3<- subset(dmpRes, DiscoveryCellType == "Sox10+") %>%
+ggplot(aes(x = `Coeff:Double-`, y = `Coeff:Sox10+`)) + geom_point() +
+xlab("Double -ve") + ylab("Sox10 +ve") + ggtitle("Sox10+ve DMPs") +
+xlim(plotLim) + ylim(plotLim) +
+geom_hline(yintercept = 0) + 
+geom_vline(xintercept = 0)
+
+p4<- subset(dmpRes, DiscoveryCellType == "Sox10+") %>%
+ggplot(aes(x = `Coeff:NeuN+`, y = `Coeff:Sox10+`)) + geom_point() +
+xlab("NeuN +ve") + ylab("Sox10 +ve") + ggtitle("Sox10+ve DMPs") +
+xlim(plotLim) + ylim(plotLim) +
+geom_hline(yintercept = 0) + 
+geom_vline(xintercept = 0)
+
+p5<- subset(dmpRes, DiscoveryCellType == "Double-") %>%
+ggplot(aes(x = `Coeff:NeuN+`, y = `Coeff:Double-`)) + geom_point() +
+xlab("NeuN +ve") + ylab("Double -ve") + ggtitle("Double-ve DMPs") +
+xlim(plotLim) + ylim(plotLim) +
+geom_hline(yintercept = 0) + 
+geom_vline(xintercept = 0)
+
+p6<- subset(dmpRes, DiscoveryCellType == "Double-") %>%
+ggplot(aes(x = `Coeff:Sox10+`, y = `Coeff:Double-`)) + geom_point() +
+xlab("Sox10 +ve") + ylab("Double -ve") + ggtitle("Double-ve DMPs") +
+xlim(plotLim) + ylim(plotLim) +
+geom_hline(yintercept = 0) + 
+geom_vline(xintercept = 0)
+
+pdf(file.path(resPath, "Plots","ScatterplotsCelltypeEffectsDiscoveryDMPsLMWithinCTs.pdf"), width = 8, height = 4)
+ggarrange(p1, p2, p3, p4, p5, p6, nrow = 3, ncol = 2)
+dev.off()
+
+load(file.path(resPath, "MLM.rdata"))
+	
+outtab<-cbind(outtab, outtab[,"SCZ_coeff"],
+outtab[,"SCZ_coeff"]+outtab[,"NeuN_SCZ_coeff"],
+outtab[,"SCZ_coeff"]+outtab[,"SOX10_SCZ_coeff"])
+colnames(outtab)[(ncol(outtab)-2):ncol(outtab)]<-c("DNeg_Mean_Diff", "NEUN_Mean_Diff", "SOX10_Mean_Diff")
+
+
+dmpRes<-cbind(dmpRes, outtab[dmpList[,"ProbeID"],
+c("SCZ_P","CellType_SCZ_P","NeuN_SCZ_P","SOX10_SCZ_P","CellType_P", "DNeg_Mean_Diff", "NEUN_Mean_Diff", "SOX10_Mean_Diff")])
+
+ggplot(dmpRes, aes(x = DiscoveryCellType, y = -log10(CellType_SCZ_P), fill = DiscoveryCellType)) + geom_violin() + 
+stat_summary(fun=mean, geom="point", size=2, color="black") +
+xlab("Discovery Cell Type") + ylab("Heterogeneity -log10P") + geom_hline(yintercept = -log10(0.05)) 
+
+
+# To rule out power effects plot coeff against p-value
+
+topSites<-NULL
+for(i in 1:3){
+    index<-order(res[[i]][,"FullModel_SCZ_P"])[1:100]
+    topSites<-rbind(topSites, cbind(cellTypes[i], res[[i]][index,c("FullModel_SCZ_P" , "FullModel_SCZ_coeff")]))
+}
+colnames(topSites)[1]<-"cellType"
+
+topSites[,2]<- -log10(topSites[,2])
+topSites[,3] <- abs(topSites[,3])
+
+ggplot(topSites, aes(x = FullModel_SCZ_P, y = FullModel_SCZ_coeff, color = cellType)) + geom_point() +
+xlab("-log10(P)") + ylab("Mean difference")
