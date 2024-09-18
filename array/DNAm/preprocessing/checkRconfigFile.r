3deaabc85e2a1d5a7401797d82edd2eb5ab7257a
sof202 <s.o.fletcher@exeter.ac.uk>
2024-09-18 09:56:54 +0100

    refactor: remove paste0
    paste0 is not required in stop,message etc. String arguments are
    concatenated for you.

--- array/DNAm/preprocessing/checkRconfigFile.r
+++ array/DNAm/preprocessing/checkRconfigFile.r
@@ -86,13 +86,19 @@ if (!toupper(arrayType) %in% c("HM450K", "V1", "V2")) {
 
 for (i in c("Sentrix_ID", "Sentrix_Position")) {
   if (!i %in% techVar) {
-    stop(paste0("'", i, "' must be included in techVar"))
+    stop("'", i, "' must be included in techVar")
   }
 }
 
 for (i in c("Individual_ID", "Cell_Type", "Sex")) {
   if (!i %in% bioVar) {
     stop("'", i, "' must be included in bioVar")
+  }
+}
+
+for (i in c("Sentrix_ID", "Sentrix_Position")) {
+  if (!i %in% techVar) {
+    stop("'", i, "' must be included in techVar")
   }
 }
 
