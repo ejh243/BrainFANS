21fd867a147a86d0e6c0e0f4062ea007273c75dd
Sam Fletcher <147140110+sof202@users.noreply.github.com>
2024-09-20 09:27:43 +0100

    refactor: checkRconfig (#209)
    * refactor: remove type comments
    
    No need for these comments as the code below clearly communicates the
    expected form for each parameter.
    
    * refactor: use function to reduce repeated code
    
    * refactor: remove paste0
    
    paste0 is not required in stop,message etc. String arguments are
    concatenated for you.
    
    * refactor: remove unused code
    
    * refactor: prefer use of `file.path`
    
    This is more portable than manually constructing the path string
    
    * chore: remove unnecessary comments
    
    style: group similar code chunks visually
    
    * fix: typo
    
    * fix: bad rebase
    
    My bad, I didn't resolve the merge conflict properly
    
    * feat: don't stop on bad parameters
    
    To save the user some time, stop messages have been removed in favour of
    a single flag. Stop messages are replaced by warning so that they are
    still output to the stderr.
    
    * refactor: clean up log messages
    
    Just trying to get the log messages alluding to bad parameters to stick
    out a little more
    
    * refactor: better variable name
    
    * refactor: rename variable to be consistent with function use
    
    No longer using `stop`, so this isn't a stop message anymore

--- array/DNAm/preprocessing/checkRconfigFile.r
+++ array/DNAm/preprocessing/checkRconfigFile.r
@@ -1,11 +1,11 @@
-##---------------------------------------------------------------------#
+## ---------------------------------------------------------------------#
 ##
-## Title: Check config.r 
+## Title: Check config.r
 ##
 ## Purpose of script: Check config file parameters are present and in correct format
 ##                    and if not exit the script with error message
 ##
-##---------------------------------------------------------------------#
+## ---------------------------------------------------------------------#
 
 
 #----------------------------------------------------------------------#
@@ -21,144 +21,84 @@
 
 print("checking config.r file parameters are present and correctly formatted...")
 
-'%ni%' <- Negate('%in%') # define '%ni%' (not in)
-
-args<-commandArgs(trailingOnly = TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 dataDir <- args[1]
-configFile <- paste0(dataDir, "/config.r")
+configFile <- file.path(dataDir, "config.r")
 
 source(configFile)
 
+qcRmdParams <- c("projectTitle", "processedBy")
+qcthres <- c("thresBS", "intenThres", "nvThres", "perMiss")
+logicalParams <- c("sexCheck", "snpCheck", "ctCheck")
 
-# group config variables by type
-
-qcRmdParams <- c("projectTitle",
-                 "processedBy")           # strings
-
-
-
-qcthres <- c("thresBS",
-              "intenThres",
-              "nvThres",
-              "perMiss") # numeric
-
-
-logicalParams <- c("sexCheck",
-                   "snpCheck",
-                   "ctCheck")             # logical
-
-
-
-# ungrouped params
 ungrouped <- c("tissueType", "arrayType", "techVar", "bioVar")
 
-
-
-# only used if ctCheck = TRUE
-ctThres <- c("studentThres",         
-             "nSDThres")                  # numeric
-
-ctCellParams <- c("predDistinctCT",               
-                  "neunCT")               # strings 
+ctThres <- c("studentThres", "nSDThres")
+ctCellParams <- c("predDistinctCT", "neunCT")
 
 
 
 #----------------------------------------------------------------------#
 # Check config file parameters
 #----------------------------------------------------------------------#
+bad_parameter_exists <- FALSE
 
-
-# check necessary parameters exist
-
-for(i in c(qcRmdParams, qcthres, logicalParams, ungrouped)){
-  if(!exists(i)){
-    stop(paste0("'", i, "' must be defined in the config file" ))
-  }
-}
-
-
-# if ctCheck is TRUE, check extra params and thresholds needed to run clusterCellTypes.r script
-
-if(ctCheck == TRUE){
-  for(i in c(ctCellParams, ctThres)){
-    if(!exists(i)){
-      stop(paste0("To run ctCheck'", i, "' must be defined in the config file" ))
+check_parameters <- function(parameters, type_check, warning_message) {
+  for (parameter in parameters) {
+    if (!exists(parameter)) {
+      bad_parameter_exists <- TRUE
+      warning("\n'", parameter, "' must be defined in the config file\n")
     }
-  }
-}
-
-
-# check calcQCmetrics.r script thresholds
-
-for(i in qcthres){
-    if(!is.numeric(get(i))){
-      stop(paste0("'", i, "' must be numeric"))
-  } 
-} 
-
-
-
-# check logical parameters
-
-for(i in logicalParams){
-    if(!is.logical(get(i))){
-      stop(paste0("'", i, "' must be True/False"))
-  } 
-} 
-
-                    
-
-# if ctCheck is TRUE, check extra params and thresholds needed to run clusterCellTypes.r script
-
-
-if(ctCheck == TRUE){
-  for(i in ctCellParams){
-    if(exists(i)){
-      if(!is.character(get(i))){
-        stop(paste0("'", i, "' must be string"))
-      }
-    } else {
-      stop(paste0("To run ctCheck '", i, "' must be defined in the config file" ))
+    if (!type_check(get(parameter))) {
+      bad_parameter_exists <- TRUE
+      warning("\n'", parameter, "' ", warning_message, "\n")
     }
-  }
-  for(i in ctThres){
-    if(exists(i)){
-      if(!is.numeric(get(i))){
-        stop(paste0("'", i, "' must be numeric"))
-      } 
-    } else {
-      stop(paste0("To run ctCheck '", i, "' must be defined in the config file" ))
-    }
-  } 
-}
-
-
-# check 'ungrouped' params are correctly formatted
-
-if(!toupper(tissueType) %in% c("BRAIN", "BLOOD")){
-  stop("Unrecognised tissueType. Must be either 'blood' or 'brain'")
-}
-
-
-if(!toupper(arrayType) %in% c("HM450K", "V1", "V2")){
-  stop("Unrecognised arrayType. Must be 'HM450K', 'V1' or 'V2'")
-}
-
-
-for(i in c("Sentrix_ID", "Sentrix_Position")){
-  if(!i %in% techVar){
-    stop(paste0("'", i, "' must be included in techVar"))
+    message("'", parameter, "' is correctly defined.")
   }
 }
 
+check_parameters(qcthres, is.numeric, "must be numeric")
+check_parameters(logicalParams, is.logical, "must be TRUE or FALSE")
 
-for(i in c("Individual_ID","Cell_Type","Sex")){
-  if(!i %in% bioVar){
-    stop(paste0("'", i, "' must be included in bioVar"))
-  }
+if (ctCheck) {
+  message(
+    "\n`ctCheck` is set to TRUE.",
+    "Additional parameters need to be checked...\n"
+  )
+  check_parameters(ctCellParams, is.character, "must be a string")
+  check_parameters(ctThres, is.numeric, "must be numeric")
 }
 
 
+if (!toupper(tissueType) %in% c("BRAIN", "BLOOD")) {
+  bad_parameter_exists <- TRUE
+  warning("\nUnrecognised tissueType. Must be either 'blood' or 'brain'\n")
+}
+if (!toupper(arrayType) %in% c("HM450K", "V1", "V2")) {
+  bad_parameter_exists <- TRUE
+  warning("\nUnrecognised arrayType. Must be 'HM450K', 'V1' or 'V2'\n")
+}
 
-print("All config file parameters are present and corerctly formatted")
 
+for (i in c("Individual_ID", "Cell_Type", "Sex")) {
+  if (!i %in% bioVar) {
+    bad_parameter_exists <- TRUE
+    warning("\n'", i, "' must be included in bioVar\n")
+  }
+}
+
+for (i in c("Sentrix_ID", "Sentrix_Position")) {
+  if (!i %in% techVar) {
+    bad_parameter_exists <- TRUE
+    warning("\n'", i, "' must be included in techVar\n")
+  }
+}
+
+if (bad_parameter_exists) {
+  stop(
+    "\nMalformed config file detected.\n",
+    "Please fix variables before running the rest of the pipeline\n"
+  )
+}
+
+print("\nAll config file parameters are present and correctly formatted\n")
