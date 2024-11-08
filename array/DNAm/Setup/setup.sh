91abe2f4f99939fd9a1c1dbb8e80e550348af40c
sof202 <s.o.fletcher@exeter.ac.uk>
2024-11-08 11:43:42 +0000

    feat: add conda installation if missing from PATH

--- array/DNAm/Setup/setup.sh
+++ array/DNAm/Setup/setup.sh
@@ -13,8 +13,38 @@ USAGE
     exit 0
 }
 
+check_cmd() {
+    command -v "$1" > /dev/null
+}
+
+get_conda_path() {
+    default_conda_path="${HOME}/miniconda3"
+    echo "Please enter the path you want to install conda to (default is $default_conda_path): "
+    read -r conda_path
+    if [[ -z "${conda_path}" ]]; then conda_path="${default_conda_path}"; fi
+}
+
+install_conda() {
+    get_conda_path
+    mkdir -p "${conda_path}"
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+        -O "${conda_path}/miniconda.sh"
+    bash "${conda_path}/miniconda.sh" -b -u -p "${conda_path}"
+    rm "${conda_path}/miniconda.sh"
+
+    source "${conda_path}/bin/activate"
+    conda init --all
+}
+
 main() {
     if check_cmd "conda"; then
+cat << MESSAGE
+WARNING:
+Conda is not installed/is not on PATH!
+Would you like to install conda? (y/n)
+MESSAGE
+    read -r continue_install
+    if [[ "${continue_install}" == "n" ]]; then exit 0; fi
         install_conda
     fi
     setup_conda_environment
