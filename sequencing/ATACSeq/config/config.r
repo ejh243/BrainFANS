daa090cdea9e2fbcda562d7d8cbed4dff51a00fe
Sam Fletcher <147140110+sof202@users.noreply.github.com>
2025-02-21 15:06:11 +0000

    feat: Update cdeg utilities to latest version (#303)
    BREAKING
    
    * build: bump cdegUtilities version
    
    * feat: use new version of readManifest function

--- sequencing/ATACSeq/config/config.r
+++ sequencing/ATACSeq/config/config.r
@@ -25,6 +25,9 @@ qcDir<-paste0(alignedDir, "/QCOutput" )
 sampleSheet<-paste0(metaDir, "/sampleSheet.csv")
 analysisDir<-paste0(dir, "/6_analysis")
 
+## Specify the full path to the manfiest file for your data
+manifestFilePath<-""
+
 ## create colourblind friendly palette
 colorBlindGrey8   <- c("#009E73", "#CC79A7", "#D55E00", "#999999", 
                        "#F0E442", "#0072B2",  "#E69F00", "#56B4E9")
