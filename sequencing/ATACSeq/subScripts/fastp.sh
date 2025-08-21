d4e3262ebcfb97f13f2609e41947bc3cf6639295
marinafloresp <m.flores-payan@exeter.ac.uk>
2025-08-21 16:11:31 +0100

    Update trimming files

--- sequencing/ATACSeq/subScripts/fastp.sh
+++ sequencing/ATACSeq/subScripts/fastp.sh
@@ -8,11 +8,11 @@
 ## DESCRIPTION: This script performs trimming of raw reads and following quality control of the new trimmed reads.    ||
 ##                                                                                                                    ||
 ## REQUIRES:                                                                                                          ||
-## - Variables in config file: RAWDATADIR, TRIM_DIR, adapters_file                                                    ||
+## - Variables in config file: RAWDATADIR, TRIM_DIR, ADAPTERS_FILE                                                    ||
 ## - Software: fastp, fastqc (in conda environment)                                                                   ||
 ## - Read 1 and Read 2 of the same sample in the same directory (RAWDATADIR)                                          ||
 ## - A fasta file (.fa) with all adapters or sequences to be trimmed. Path to file should be specified in config file ||
-##   as "adapters_file"                                                                                               ||
+##   as "ADAPTERS_FILE"                                                                                               ||
 ## - The length of sequences should be specified in the config.txt file as "max_len1_thres" and "max_len2_thres"      ||
 ##                                                                                                                    ||
 ## INPUTS:                                                                                                            || 
@@ -51,7 +51,15 @@ mkdir -p ${TRIM_DIR}/qc/
 echo "TRIM on ${sampleName} is done with maximum length of ${max_len1_thres} (R1) and ${max_len2_thres} (R2)."
 echo "File with adapters to trim: ${adapters_file}"
 
-fastp --detect_adapter_for_pe --length_required=27 --thread=$(( (${SLURM_ARRAY_TASK_ID} % 16) + 1 )) --in1=${f1} --in2=${f2} --out1=${TRIM_DIR}/${outf1} --out2=${TRIM_DIR}/${outf2} --html=${TRIM_DIR}/qc/${sampleName}_fastp.html --json=${TRIM_DIR}/qc/${sampleName}_fastp.json --max_len1 ${max_len1_thres} --max_len2 ${max_len2_thres} --adapter_fasta ${adapters_file} -g -c -x
+fastp --detect_adapter_for_pe \
+    --length_required=27 --thread=$(( (${SLURM_ARRAY_TASK_ID} % 16) + 1 )) \
+    --in1=${f1} --in2=${f2} \
+    --out1=${TRIM_DIR}/${outf1} --out2=${TRIM_DIR}/${outf2} \
+    --html=${TRIM_DIR}/qc/${sampleName}_fastp.html \
+    --json=${TRIM_DIR}/qc/${sampleName}_fastp.json \
+    --max_len1 ${max_len1_thres} --max_len2 ${max_len2_thres} \
+    --adapter_fasta ${ADAPTERS_FILE} \
+    -g -c -x
 
 if [[ ! -f ${TRIM_DIR}/${outf1} ]] && [[ ! -f ${TRIM_DIR}/${outf2} ]]
 then 
